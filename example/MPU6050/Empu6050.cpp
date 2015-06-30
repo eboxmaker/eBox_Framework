f574a25f7fd75dfed9aeb37d3f6206c4b3534233
shentqlf <shentqlf@163.com>
2015-07-01 02:07:33 +0800

    modefied mpu6050

--- example/MPU6050/Empu6050.cpp
+++ example/MPU6050/Empu6050.cpp
@@ -21,21 +21,27 @@ void setup()
 
 int16_t tmp[7];
 int16_t x,y,z;
+int16_t id;
 int main(void)
 {
 	setup();
 	
 	while(1)
 	{
-		mpu.getData(ACCEL_XOUT_H,tmp,3);
+		id = mpu.getID();
+		mpu.getData(ACCEL_XOUT_H,tmp,7);
 //		x = mpu.getData(ACCEL_XOUT_H);
 //		y = mpu.getData(ACCEL_YOUT_H);
 //		z = mpu.getData(ACCEL_ZOUT_H);
-		uart3.printf("\r\nx = %d",tmp[0]);
-		uart3.printf("\r\nx = %d",tmp[1]);
-		uart3.printf("\r\nx = %d",tmp[2]);
+		uart3.printf("\r\nid = %d",id);
+		uart3.printf("\r\naccx = %d",tmp[0]);
+		uart3.printf("\r\naccy = %d",tmp[1]);
+		uart3.printf("\r\naccz = %d",tmp[2]);
+		uart3.printf("\r\ntemp = %d",tmp[3]);
+		uart3.printf("\r\ngyrox = %d",tmp[4]);
+		uart3.printf("\r\ngyroy = %d",tmp[5]);
+		uart3.printf("\r\ngyroz = %d",tmp[6]);
 		uart3.printf("\r\n==========");
-		
 //		uart3.printf("\r\nX = %d",x);
 //		uart3.printf("\r\nY = %d",y);
 //		uart3.printf("\r\nZ = %d",z);
