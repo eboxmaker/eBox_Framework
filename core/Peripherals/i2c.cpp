c99869538b3420a22ae1e215f9e3e704724449f4
shentqlf <shentqlf@163.com>
2015-07-02 12:56:06 +0800

    modefied i2c

--- core/Peripherals/i2c.cpp
+++ core/Peripherals/i2c.cpp
@@ -1,39 +1,16 @@
 #include "i2c.h"
 
-/* MPU6050 Register Address ------------------------------------------------------------*/
-#define	SMPLRT_DIV		0x19	//陀螺仪采样率，典型值：0x07(125Hz)
-#define	CONFIG				0x1A	//低通滤波频率，典型值：0x06(5Hz)
-#define	GYRO_CONFIG		0x1B	//陀螺仪自检及测量范围，典型值：0x18(不自检，2000deg/s)
-#define	ACCEL_CONFIG	0x1C	//加速计自检、测量范围及高通滤波频率，典型值：0x01(不自检，2G，5Hz)
-#define	ACCEL_XOUT_H	0x3B
-#define	ACCEL_XOUT_L	0x3C
-#define	ACCEL_YOUT_H	0x3D
-#define	ACCEL_YOUT_L	0x3E
-#define	ACCEL_ZOUT_H	0x3F
-#define	ACCEL_ZOUT_L	0x40
-#define	TEMP_OUT_H		0x41
-#define	TEMP_OUT_L		0x42
-#define	GYRO_XOUT_H		0x43
-#define	GYRO_XOUT_L		0x44	
-#define	GYRO_YOUT_H		0x45
-#define	GYRO_YOUT_L		0x46
-#define	GYRO_ZOUT_H		0x47
-#define	GYRO_ZOUT_L		0x48
-#define	PWR_MGMT_1		0x6B	//电源管理，典型值：0x00(正常启用)
-#define	WHO_AM_I			0x75	//IIC地址寄存器(默认数值0x68，只读)
-#define	SlaveAddress	0xD0	//IIC写入时的地址字节数据
 
 void  I2C::i2cBegin()
 {
 	
-	GPIO_InitTypeDef GPIO_InitStructure;
 	
-		/* 使能与 I2C1 有关的时钟 */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1,ENABLE);  
 
 	pinMode(_SDAPin,AF_OD);
 	pinMode(_SCLPin,AF_OD);
 
+		/* 使能与 I2C1 有关的时钟 */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1,ENABLE);  
 
 	  /* I2C 配置 */
 	I2C_InitStructure.I2C_Mode = I2C_Mode_I2C ; 
@@ -131,7 +108,7 @@ int8_t I2C::readByte(uint8_t slaveAddress,uint8_t regAddress,uint8_t* data)
 	I2C_Cmd(I2C1,ENABLE);
 	sendByte(regAddress);
 	start();
-	send7BitsAddress(SlaveAddress + 1);
+	send7BitsAddress(slaveAddress + 1);
 	sendNoAck();
 	stop();
 	*data = receiveByte();
@@ -147,7 +124,7 @@ int8_t I2C::readByte(uint8_t slaveAddress,uint8_t regAddress,uint8_t* data,uint8
 	I2C_Cmd(I2C1,ENABLE);
 	sendByte(regAddress);
 	start();
-	send7BitsAddress(SlaveAddress + 1);
+	send7BitsAddress(slaveAddress + 1);
 	
 		while(numToRead)
 		{
