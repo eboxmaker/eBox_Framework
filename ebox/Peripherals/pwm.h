0a3d372ed812275c728c5c767012120d143f6cdd
shentqlf <shentqlf@163.com>
2015-07-08 10:50:34 +0800

    modefied copyright

--- ebox/Peripherals/pwm.h
+++ ebox/Peripherals/pwm.h
@@ -4,13 +4,13 @@ author : shentq
 version: V1.0
 date   : 2015/7/5
 
-Copyright (c) 2015, eBox by shentqlf@163.com. All Rights Reserved.
+Copyright (c) 2015, eBox by shentq. All Rights Reserved.
 
 Copyright Notice
-No part of this software may be used for any commercial activities by any form or means, without the prior written consent of shentqlf@163.com.
+No part of this software may be used for any commercial activities by any form or means, without the prior written consent of shentq.
 
 Disclaimer
-This specification is preliminary and is subject to change at any time without notice. shentqlf@163.com assumes no responsibility for any errors contained herein.
+This specification is preliminary and is subject to change at any time without notice. shentq assumes no responsibility for any errors contained herein.
 */
 #ifndef __PWM_H
 #define __PWM_H
