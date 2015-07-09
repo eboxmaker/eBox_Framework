b5973b09cb86c87825501e28fe60bda8190532e8
shentqlf <shentqlf@163.com>
2015-07-09 20:03:30 +0800

    io.cpp修改为digital.cpp

--- ebox/core/digital.cpp
+++ ebox/core/digital.cpp
@@ -1,210 +1,168 @@
-/*
-file   : digital.cpp
-author : shentq
-version: V1.0
-date   : 2015/7/5
-brief  :   digital input and output function
-
-Copyright (c) 2015, eBox by shentq. All Rights Reserved.
-
-Copyright Notice
-No part of this software may be used for any commercial activities by any form or means, without the prior written consent of shentq.
-
-Disclaimer
-This specification is preliminary and is subject to change at any time without notice. shentq assumes no responsibility for any errors contained herein.
-*/
-
 #include "common.h"
+//GPIO GPA0(GPIOA,GPIO_Pin_0);
+//GPIO GPA1(GPIOA,GPIO_Pin_1);
+//GPIO GPA2(GPIOA,GPIO_Pin_2);
+//GPIO GPA3(GPIOA,GPIO_Pin_3);
+//GPIO GPA4(GPIOA,GPIO_Pin_4);
+//GPIO GPA5(GPIOA,GPIO_Pin_5);
+//GPIO GPA6(GPIOA,GPIO_Pin_6);
+//GPIO GPA7(GPIOA,GPIO_Pin_7);
+//GPIO GPA8(GPIOA,GPIO_Pin_8);
+//GPIO GPA9(GPIOA,GPIO_Pin_9);
+//GPIO GPA10(GPIOA,GPIO_Pin_10);
+//GPIO GPA11(GPIOA,GPIO_Pin_11);
+//GPIO GPA12(GPIOA,GPIO_Pin_12);
+//GPIO GPA13(GPIOA,GPIO_Pin_13);
+//GPIO GPA14(GPIOA,GPIO_Pin_14);
+//GPIO GPA15(GPIOA,GPIO_Pin_15);
 
-//uint16_t ioPinStatu[TOTAL_PIN];//IO口是否使用
-//	#define TOTAL_PIN 48
+//GPIO GPB0(GPIOB,GPIO_Pin_0);
+//GPIO GPB1(GPIOB,GPIO_Pin_1);
+//GPIO GPB2(GPIOB,GPIO_Pin_2);
+//GPIO GPB3(GPIOB,GPIO_Pin_3);
+//GPIO GPB4(GPIOB,GPIO_Pin_4);
+//GPIO GPB5(GPIOB,GPIO_Pin_5);
+//GPIO GPB6(GPIOB,GPIO_Pin_6);
+//GPIO GPB7(GPIOB,GPIO_Pin_7);
+//GPIO GPB8(GPIOB,GPIO_Pin_8);
+//GPIO GPB9(GPIOB,GPIO_Pin_9);
+//GPIO GPB10(GPIOB,GPIO_Pin_10);
+//GPIO GPB11(GPIOB,GPIO_Pin_11);
+//GPIO GPB12(GPIOB,GPIO_Pin_12);
+//GPIO GPB13(GPIOB,GPIO_Pin_13);
+//GPIO GPB14(GPIOB,GPIO_Pin_14);
+//GPIO GPB15(GPIOB,GPIO_Pin_15);
 
-typedef struct 
+//GPIO GPC0(GPIOC,GPIO_Pin_0);
+//GPIO GPC1(GPIOC,GPIO_Pin_1);
+//GPIO GPC2(GPIOC,GPIO_Pin_2);
+//GPIO GPC3(GPIOC,GPIO_Pin_3);
+//GPIO GPC4(GPIOC,GPIO_Pin_4);
+//GPIO GPC5(GPIOC,GPIO_Pin_5);
+//GPIO GPC6(GPIOC,GPIO_Pin_6);
+//GPIO GPC7(GPIOC,GPIO_Pin_7);
+//GPIO GPC8(GPIOC,GPIO_Pin_8);
+//GPIO GPC9(GPIOC,GPIO_Pin_9);
+//GPIO GPC10(GPIOC,GPIO_Pin_10);
+//GPIO GPC11(GPIOC,GPIO_Pin_11);
+//GPIO GPC12(GPIOC,GPIO_Pin_12);
+//GPIO GPC13(GPIOC,GPIO_Pin_13);
+//GPIO GPC14(GPIOC,GPIO_Pin_14);
+//GPIO GPC15(GPIOC,GPIO_Pin_15);
+
+//GPIO GPD0(GPIOD,GPIO_Pin_0);
+//GPIO GPD1(GPIOD,GPIO_Pin_1);
+//GPIO GPD2(GPIOD,GPIO_Pin_2);
+//GPIO GPD3(GPIOD,GPIO_Pin_3);
+//GPIO GPD4(GPIOD,GPIO_Pin_4);
+//GPIO GPD5(GPIOD,GPIO_Pin_5);
+//GPIO GPD6(GPIOD,GPIO_Pin_6);
+//GPIO GPD7(GPIOD,GPIO_Pin_7);
+//GPIO GPD8(GPIOD,GPIO_Pin_8);
+//GPIO GPD9(GPIOD,GPIO_Pin_9);
+//GPIO GPD10(GPIOD,GPIO_Pin_10);
+//GPIO GPD11(GPIOD,GPIO_Pin_11);
+//GPIO GPD12(GPIOD,GPIO_Pin_12);
+//GPIO GPD13(GPIOD,GPIO_Pin_13);
+//GPIO GPD14(GPIOD,GPIO_Pin_14);
+//GPIO GPD15(GPIOD,GPIO_Pin_15);
+
+
+
+//GPIO* PC0 = new GPIO(GPIOC,GPIO_Pin_0);
+
+GPIO::GPIO(GPIO_TypeDef* _port,uint16_t _pin)
 {
-  GPIO_TypeDef* port ;
-  uint32_t pin ;
-  uint32_t rcc ;
-} PIN_INFO ;
-
-const PIN_INFO pinInfo[]=
-{
-	//0-15
-  { GPIOA, GPIO_Pin_0,  RCC_APB2Periph_GPIOA}, //1
-  { GPIOA, GPIO_Pin_1,  RCC_APB2Periph_GPIOA}, // 2
-  { GPIOA, GPIO_Pin_2,  RCC_APB2Periph_GPIOA}, // 3
-  { GPIOA, GPIO_Pin_3,  RCC_APB2Periph_GPIOA}, //4
-  { GPIOA, GPIO_Pin_4,  RCC_APB2Periph_GPIOA}, // 5
-  { GPIOA, GPIO_Pin_5,  RCC_APB2Periph_GPIOA}, //6
-  { GPIOA, GPIO_Pin_6,  RCC_APB2Periph_GPIOA}, // 7
-  { GPIOA, GPIO_Pin_7,  RCC_APB2Periph_GPIOA}, //8
-  { GPIOA, GPIO_Pin_8,  RCC_APB2Periph_GPIOA}, // 9
-  { GPIOA, GPIO_Pin_9,  RCC_APB2Periph_GPIOA}, //10
-  { GPIOA, GPIO_Pin_10,  RCC_APB2Periph_GPIOA}, // 11
-  { GPIOA, GPIO_Pin_11,  RCC_APB2Periph_GPIOA}, //12
-  { GPIOA, GPIO_Pin_12,  RCC_APB2Periph_GPIOA}, // 13
-  { GPIOA, GPIO_Pin_13,  RCC_APB2Periph_GPIOA}, //14
-  { GPIOA, GPIO_Pin_14,  RCC_APB2Periph_GPIOA}, // 15
-  { GPIOA, GPIO_Pin_15,  RCC_APB2Periph_GPIOA}, //16
-
-	
-	
-	//16-31
-  { GPIOB, GPIO_Pin_0,  RCC_APB2Periph_GPIOB}, //16 
-  { GPIOB, GPIO_Pin_1,  RCC_APB2Periph_GPIOB}, //17 
-  { GPIOB, GPIO_Pin_2,  RCC_APB2Periph_GPIOB}, //18 
-  { GPIOB, GPIO_Pin_3,  RCC_APB2Periph_GPIOB}, // 19
-  { GPIOB, GPIO_Pin_4,  RCC_APB2Periph_GPIOB}, // 20
-  { GPIOB, GPIO_Pin_5,  RCC_APB2Periph_GPIOB}, // 21
-  { GPIOB, GPIO_Pin_6,  RCC_APB2Periph_GPIOB}, // 22
-  { GPIOB, GPIO_Pin_7,  RCC_APB2Periph_GPIOB}, // 23
-  { GPIOB, GPIO_Pin_8,  RCC_APB2Periph_GPIOB}, // 24
-  { GPIOB, GPIO_Pin_9,  RCC_APB2Periph_GPIOB}, // 25
-  { GPIOB, GPIO_Pin_10,  RCC_APB2Periph_GPIOB}, // 26
-  { GPIOB, GPIO_Pin_11,  RCC_APB2Periph_GPIOB}, // 27
-  { GPIOB, GPIO_Pin_12,  RCC_APB2Periph_GPIOB}, //28 
-  { GPIOB, GPIO_Pin_13,  RCC_APB2Periph_GPIOB}, // 29
-  { GPIOB, GPIO_Pin_14,  RCC_APB2Periph_GPIOB}, // 30
-  { GPIOB, GPIO_Pin_15,  RCC_APB2Periph_GPIOB}, // 31
-	
-	//32-47
-	{ GPIOC, GPIO_Pin_0,  RCC_APB2Periph_GPIOC}, // 32
-  { GPIOC, GPIO_Pin_1,  RCC_APB2Periph_GPIOC}, //33 
-  { GPIOC, GPIO_Pin_2,  RCC_APB2Periph_GPIOC}, //34 
-  { GPIOC, GPIO_Pin_3,  RCC_APB2Periph_GPIOC}, //35 
-  { GPIOC, GPIO_Pin_4,  RCC_APB2Periph_GPIOC}, //36 
-  { GPIOC, GPIO_Pin_5,  RCC_APB2Periph_GPIOC}, //37 
-  { GPIOC, GPIO_Pin_6,  RCC_APB2Periph_GPIOC}, //38 
-  { GPIOC, GPIO_Pin_7,  RCC_APB2Periph_GPIOC}, //39 
-  { GPIOC, GPIO_Pin_8,  RCC_APB2Periph_GPIOC}, //40 
-  { GPIOC, GPIO_Pin_9,  RCC_APB2Periph_GPIOC}, //41 
-  { GPIOC, GPIO_Pin_10,  RCC_APB2Periph_GPIOC}, //42 
-  { GPIOC, GPIO_Pin_11,  RCC_APB2Periph_GPIOC}, //43 
-  { GPIOC, GPIO_Pin_12,  RCC_APB2Periph_GPIOC}, // 44
-  { GPIOC, GPIO_Pin_13,  RCC_APB2Periph_GPIOC}, //45 
-  { GPIOC, GPIO_Pin_14,  RCC_APB2Periph_GPIOC}, //46 
-  { GPIOC, GPIO_Pin_15,  RCC_APB2Periph_GPIOC}, // 47
-	
-	//48-63
-	{ GPIOD, GPIO_Pin_0,  RCC_APB2Periph_GPIOD}, //48 
-  { GPIOD, GPIO_Pin_1,  RCC_APB2Periph_GPIOD}, //49 
-  { GPIOD, GPIO_Pin_2,  RCC_APB2Periph_GPIOD}, //50 
-  { GPIOD, GPIO_Pin_3,  RCC_APB2Periph_GPIOD}, // 51
-  { GPIOD, GPIO_Pin_4,  RCC_APB2Periph_GPIOD}, // 52
-  { GPIOD, GPIO_Pin_5,  RCC_APB2Periph_GPIOD}, // 53
-  { GPIOD, GPIO_Pin_6,  RCC_APB2Periph_GPIOD}, // 54
-  { GPIOD, GPIO_Pin_7,  RCC_APB2Periph_GPIOD}, // 55
-  { GPIOD, GPIO_Pin_8,  RCC_APB2Periph_GPIOD}, // 56
-  { GPIOD, GPIO_Pin_9,  RCC_APB2Periph_GPIOD}, //57 
-  { GPIOD, GPIO_Pin_10,  RCC_APB2Periph_GPIOD}, //58 
-  { GPIOD, GPIO_Pin_11,  RCC_APB2Periph_GPIOD}, //59 
-  { GPIOD, GPIO_Pin_12,  RCC_APB2Periph_GPIOD}, //60 
-  { GPIOD, GPIO_Pin_13,  RCC_APB2Periph_GPIOD}, //61 
-  { GPIOD, GPIO_Pin_14,  RCC_APB2Periph_GPIOD}, //62 
-  { GPIOD, GPIO_Pin_15,  RCC_APB2Periph_GPIOD}, // 63	
-
-	//64-79
-	{ GPIOE, GPIO_Pin_0,  RCC_APB2Periph_GPIOE}, // 64
-  { GPIOE, GPIO_Pin_1,  RCC_APB2Periph_GPIOE}, // 65
-  { GPIOE, GPIO_Pin_2,  RCC_APB2Periph_GPIOE}, // 66
-  { GPIOE, GPIO_Pin_3,  RCC_APB2Periph_GPIOE}, // 67
-  { GPIOE, GPIO_Pin_4,  RCC_APB2Periph_GPIOE}, // 68
-  { GPIOE, GPIO_Pin_5,  RCC_APB2Periph_GPIOE}, // 69
-  { GPIOE, GPIO_Pin_6,  RCC_APB2Periph_GPIOE}, // 70
-  { GPIOE, GPIO_Pin_7,  RCC_APB2Periph_GPIOE}, // 71
-  { GPIOE, GPIO_Pin_8,  RCC_APB2Periph_GPIOE}, // 72
-  { GPIOE, GPIO_Pin_9,  RCC_APB2Periph_GPIOE}, // 73
-  { GPIOE, GPIO_Pin_10,  RCC_APB2Periph_GPIOE}, // 74
-  { GPIOE, GPIO_Pin_11,  RCC_APB2Periph_GPIOE}, // 75
-  { GPIOE, GPIO_Pin_12,  RCC_APB2Periph_GPIOE}, //76 
-  { GPIOE, GPIO_Pin_13,  RCC_APB2Periph_GPIOE}, // 77
-  { GPIOE, GPIO_Pin_14,  RCC_APB2Periph_GPIOE}, // 78
-  { GPIOE, GPIO_Pin_15,  RCC_APB2Periph_GPIOE}, // 79	
-	//80-95
-	{ GPIOF, GPIO_Pin_0,  RCC_APB2Periph_GPIOF}, // 80
-  { GPIOF, GPIO_Pin_1,  RCC_APB2Periph_GPIOF}, // 81
-  { GPIOF, GPIO_Pin_2,  RCC_APB2Periph_GPIOF}, // 82
-  { GPIOF, GPIO_Pin_3,  RCC_APB2Periph_GPIOF}, // 83
-  { GPIOF, GPIO_Pin_4,  RCC_APB2Periph_GPIOF}, // 84
-  { GPIOF, GPIO_Pin_5,  RCC_APB2Periph_GPIOF}, // 85
-  { GPIOF, GPIO_Pin_6,  RCC_APB2Periph_GPIOF}, // 86
-  { GPIOE, GPIO_Pin_7,  RCC_APB2Periph_GPIOF}, // 87
-  { GPIOF, GPIO_Pin_8,  RCC_APB2Periph_GPIOF}, // 88
-  { GPIOF, GPIO_Pin_9,  RCC_APB2Periph_GPIOF}, // 89
-  { GPIOF, GPIO_Pin_10,  RCC_APB2Periph_GPIOF}, // 90
-  { GPIOF, GPIO_Pin_11,  RCC_APB2Periph_GPIOF}, // 91
-  { GPIOF, GPIO_Pin_12,  RCC_APB2Periph_GPIOF}, //92 
-  { GPIOF, GPIO_Pin_13,  RCC_APB2Periph_GPIOF}, // 93
-  { GPIOF, GPIO_Pin_14,  RCC_APB2Periph_GPIOF}, // 94
-  { GPIOF, GPIO_Pin_15,  RCC_APB2Periph_GPIOF}, // 95
-};
-
-void pinMode(uint32_t pin, uint32_t mode)
-{
-	
-//	uint8_t bit = digitalPinToBitMask(pin);
-//	uint8_t port = digitalPinToPort(pin);
-	
-//	GPIO_TypeDef *gpio_port = g_APinDescription[Pin].pPort;
-//  uint16_t gpio_pin = g_APinDescription[Pin].Pin;
-	GPIO_TypeDef * gpio_port = pinInfo[pin].port;
-	uint16_t gpio_pin = pinInfo[pin].pin;
-	
-	GPIO_InitTypeDef GPIO_InitStructure;
-	
-  RCC_APB2PeriphClockCmd(pinInfo[pin].rcc,ENABLE);
-  GPIO_InitStructure.GPIO_Pin = gpio_pin;
-
-	switch(mode)
+	port = _port;
+	pin = _pin;
+	switch((uint32_t)port)
 	{
-		/////////////////////////////////////////////////////////
-		case INPUT:
-			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-		break;
-		case INPUT_PULLUP:
-      GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-		break;
-		case INPUT_PULLDOWN:
-			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
-	//////////////////////////////////////////////////////////
-		case OUTPUT:
-			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-		  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-		break;
-		case AF_PP:
-			RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE); //
-		  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-      GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-		break;
-	  case AF_OD:
-			 RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE); //
-			 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-       GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	  break;
-		/////////////////////////////////////////////////////////////
-	  case AIN:
-			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
-    break;
+		case (uint32_t)GPIOA:
+			rcc = RCC_APB2Periph_GPIOA;
+			break;
 
-		default:
-		break ;
+		case (uint32_t)GPIOB:
+			rcc = RCC_APB2Periph_GPIOB;
+			break;
+
+		case (uint32_t)GPIOC:
+			rcc = RCC_APB2Periph_GPIOC;
+			break;
+
+		case (uint32_t)GPIOD:
+			rcc = RCC_APB2Periph_GPIOD;
+			break;
+
+		case (uint32_t)GPIOE:
+			rcc = RCC_APB2Periph_GPIOE;
+			break;
+		
+		case (uint32_t)GPIOF:
+			rcc = RCC_APB2Periph_GPIOF;
+			break;
 	}
-		GPIO_Init(gpio_port, &GPIO_InitStructure);   //初始化GPIOC端口
+}
 
-}
-void digitalWrite( uint32_t pin, uint32_t ulVal )
+void GPIO::mode(PINMODE modeVal)
 {
-  if ( ulVal == 0 )
-  {
-    pinInfo[pin].port->BRR = pinInfo[pin].pin;
-  }
-  else
-  {
-    pinInfo[pin].port->BSRR = pinInfo[pin].pin;
-  }
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	if(modeVal == _AF_OD || modeVal == _AF_PP)
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE); //
+
+	RCC_APB2PeriphClockCmd(rcc,ENABLE);
+	GPIO_InitStructure.GPIO_Pin = pin;
+	GPIO_InitStructure.GPIO_Mode = (GPIOMode_TypeDef)modeVal;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(port, &GPIO_InitStructure);   //初始化GPIOC端口
 }
- int digitalRead( uint32_t pin )
+
+void GPIO::write(uint8_t val)
 {
-	if((pinInfo[pin].port->IDR & pinInfo[pin].pin) != 0)
+	if(val == 0)
+		port->BRR = pin;
+	else
+		port->BSRR = pin;
+}
+
+void GPIO::read(uint8_t* val)
+{
+	*val = port->IDR & pin;
+}		
+
+uint8_t GPIO::read(void)
+{
+	if(port->IDR & pin)
 		return 1;
-	return 0;
+	return  0;
+}
+
+uint8_t shiftIn(GPIO* dataPin, GPIO* clockPin, uint8_t bitOrder) {
+	uint8_t value = 0;
+	uint8_t i;
+
+	for (i = 0; i < 8; ++i) {
+		clockPin->write(HIGH);
+		if (bitOrder == LSBFIRST)
+			value |= dataPin->read() << i;
+		else
+			value |= dataPin->read() << (7 - i);
+		clockPin->write(LOW);
+	}
+	return value;
+}
+
+void shiftOut(GPIO* dataPin, GPIO* clockPin, uint8_t bitOrder, uint8_t val)
+{
+	int i;
+	for (i = 0; i < 8; i++)  
+	{
+		if (bitOrder == LSBFIRST)
+			dataPin->write(!!(val & (1 << i)));
+		else	
+			dataPin->write(!!(val & (1 << (7 - i))));
+			clockPin->write(HIGH);
+			clockPin->write(LOW);
+
+	}
 }
