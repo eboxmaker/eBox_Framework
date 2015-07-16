da1c97dc8bc4b4b601a3be777fe0e6d986f4f371
shentqlf <shentqlf@163.com>
2015-07-16 23:13:53 +0800

    删除sourceinsight

--- user/stm32f10x_it.c
+++ user/stm32f10x_it.c
@@ -22,7 +22,7 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f10x_it.h"
+//#include "stm32f10x_it.h"
 
 /** @addtogroup STM32F10x_StdPeriph_Template
   * @{
