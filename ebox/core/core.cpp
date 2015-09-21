3e826e118589f229c3b098dfd8ddf3bb3c1af2db
shentqlf <shentqlf@163.com>
2015-09-21 23:24:44 +0800

    修改中断位置

--- ebox/core/core.cpp
+++ ebox/core/core.cpp
@@ -124,15 +124,103 @@ void delayus(uint32_t us)
 		}
 } 
 
+extern "C"{
+	
 #if USE_OS
 
 #else
-extern "C"{
-void SysTick_Handler(void)
-{
+	
+	void SysTick_Handler(void)//systick中断
+	{
 
-	millisSeconds++;
+		millisSeconds++;
 
-}
-}
+	}
+
+	void PendSV_Handler(void)//操作系统中切换任务使用的中断
+	{
+	}
+
 #endif
+
+//STM32其他中断////////////////////////////
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+}
