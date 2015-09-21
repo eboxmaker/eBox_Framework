438f2529d53c0db607e1f3da46064c16d80cf1b2
shentqlf <shentqlf@163.com>
2015-09-21 19:51:38 +0800

    更新文件系统examaple

--- example/fat/FAT_dir.cpp
+++ example/fat/FAT_dir.cpp
@@ -1,10 +1,8 @@
 
 #include "ebox.h"
-#include "w5500.h"
 #include "mmc_sd.h"
 #include "ff.h"
-#include "wrapperdiskio.h"
-#include "udp.h"
+
 extern void attachSDCardToFat(SD* sd);
 
 USART uart1(USART1,PA9,PA10);
@@ -19,7 +17,7 @@ SD sd(PB12,SPI2,PB13,PB14,PB15);
 
 void dirOpt()
 {
-	res=f_mkdir("/123");//新建目录只能一级一级的建，即调用一次f_mkdir(),建一层目录而已，目录名不能以数字开头
+	res=f_mkdir("0:123");//新建目录只能一级一级的建，即调用一次f_mkdir(),建一层目录而已，目录名不能以数字开头
 	if(res==FR_OK)
 		uart1.printf("\r\n创建目录成功 !");
 	else if(res==FR_EXIST)
@@ -27,7 +25,7 @@ void dirOpt()
 	else
 		uart1.printf("\r\n创建目录失败~~~~(>_<)~~~~");
 
-	res=f_opendir(&DirObject,"/123");//打开目录
+	res=f_opendir(&DirObject,"0:123");//打开目录
 	if(res==FR_OK)
 	{
 		uart1.printf("\r\n打开目录成功 !");
@@ -48,8 +46,8 @@ void setup()
 		uart1.printf("\r\nsdcard init ok!");
 	attachSDCardToFat(&sd);
 	
-	f_mount(0, &fs);
-   
+	res = f_mount(&fs,"0:",1);
+	uart1.printf("\r\nres = %d",res);
 }
 u32 count;
 int main(void)
