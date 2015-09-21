b7823ee01075899e933422f5a58fad8719823f41
shentqlf <shentqlf@163.com>
2015-09-21 20:30:11 +0800

    修改部分例程

--- example/analog/analogRead.cpp
+++ example/analog/analogRead.cpp
@@ -1,13 +1,12 @@
 
 #include "ebox.h"
 
-USART uart3(USART3,PB10,PB11);
+USART uart1(USART1,PA9,PA10);
 
 void setup()
 {
 	eBoxInit();
-	uart3.begin(9600);
-	
+	uart1.begin(9600);
 	PA7->mode(AIN);
 }
 
@@ -17,12 +16,11 @@ int main(void)
 	setup();
 	while(1)
 	{
-		
 		x = analogRead(PA7);
-		uart3.printf("hex = %05d\r\n",x);
+		uart1.printf("hex = %05d\r\n",x);
 		x = analogReadToVoltage(PA7);
-		uart3.printf("val = %04dmv\r\n",x);
-		uart3.printf("==============\r\n",x);
+		uart1.printf("val = %04dmv\r\n",x);
+		uart1.printf("==============\r\n",x);
 		delay_ms(1000);
 	}
 
--- example/digital/EdigitalWrite.cpp
+++ example/digital/EdigitalWrite.cpp
@@ -4,12 +4,8 @@
 void setup()
 {
 	eBoxInit();
-	
 	PA7->mode(OUTPUT_PP);
-//	pinMode(PA7,OUTPUT_PP);
 }
-
-int16_t x;
 int main(void)
 {
 	setup();
@@ -17,10 +13,7 @@ int main(void)
 	{
 		PA7->set();
 		PA7->reset();
-//		digitalWrite(PA7,1);
-//		digitalWrite(PA7,0);
-}
-
+	}
 
 }
 
