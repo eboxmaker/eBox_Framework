af4917ebe4eab9309d22f7c315688c5eaf0fa960
shentqlf <shentqlf@163.com>
2015-11-16 21:59:59 +0800

    增加tcpc的功能

--- network/tcp.h
+++ network/tcp.h
@@ -5,15 +5,20 @@
 class TCPCLIENT
 {
 	public:
-		int begin(SOCKET ps,uint16_t port);
-		int connect(u8* IP,uint16_t Port);
-		u16 recv(u8* buf);
-//		u16 interruptRecv(u8* buf);
-	  u16 send(u8* buf,u16 len);
+		int     begin(SOCKET ps,uint16_t port);
+		int		connect(u8* IP,uint16_t Port);
+	
+		uint8_t status();//网卡当前的状态
+		int		rx_availabe();//接收缓冲区是否可用
+		uint8_t	is_connected();//是否存在链接
+		void	stop();
+	
+		u16     recv(u8* buf);
+		u16 	send(u8* buf,u16 len);
 
-	  u8  recvFlag;
+		u8  recvFlag;
 		u16 localPort;
-	  u8  remoteIP[4];/*配置服务器的IP地址*/
+		u8  remoteIP[4];/*配置服务器的IP地址*/
 		u16 remotePort;/*定义计算机的一个端口并初始化*/
 
 
