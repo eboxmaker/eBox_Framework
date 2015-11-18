7685a42996485645d72668697bba957f719dcd45
shentqlf <shentqlf@163.com>
2015-11-18 13:02:53 +0800

    修改ws2812驱动

---
+++ example/ws2812/e_ws2812.cpp
@@ -0,0 +1,41 @@
+/*
+file   : *.cpp
+author : shentq
+version: V1.0
+date   : 2015/7/5
+
+Copyright 2015 shentq. All Rights Reserved.
+*/
+
+//STM32 RUN IN eBox
+
+
+#include "ebox.h"
+#include "math.h"
+#include "ws2812.h"
+
+WS2812 led(&PB0);
+
+
+void setup()
+{
+	ebox_init();
+	uart1.begin(9600);
+    led.begin();
+
+}
+int main(void)
+{
+	setup();
+	while(1)
+	{	
+        led.rainbow_Loop();
+
+	}
+
+}
+
+
+
+
+
