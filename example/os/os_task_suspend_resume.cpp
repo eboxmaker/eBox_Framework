52bc7d94cd29a46cbbeb2466efd5655208e894a9
shentqlf <shentqlf@163.com>
2015-12-22 01:39:03 +0800

    修改操作系统api，并完善cpu使用率统计功能

--- example/os/os_task_suspend_resume.cpp
+++ example/os/os_task_suspend_resume.cpp
@@ -22,6 +22,7 @@ void task_3();
 
 float cpu;
 INT16U mem;
+u8 task1count = 0;
 u8 task2count = 0;
 
 void setup()
@@ -32,15 +33,12 @@ void setup()
 	uart1.begin(9600);
 	uart1.printf("\r\nuart1 9600 ok!");
 	
-	uart1.printf("\r\nos初始化!");
-    PB8.mode(OUTPUT_PP);
-    PB9.mode(OUTPUT_PP);
-    PB10.mode(OUTPUT_PP);
+		uart1.printf("\r\nos初始化!");
 
 	os_task_create(task_1,&TASK_1_STK[TASK_1_STK_SIZE-1],TASK1_PRIO);
 	os_task_create(task_2,&TASK_2_STK[TASK_2_STK_SIZE-1],TASK2_PRIO);
 	os_task_create(task_3,&TASK_3_STK[TASK_3_STK_SIZE-1],TASK3_PRIO);
-			uart1.printf("\r\nos创建任务成功");
+	uart1.printf("\r\nos创建任务成功");
 
 	os_start();
 
@@ -49,18 +47,29 @@ void task_1()
 {
 	while(1)
 	{
+		task1count++;
 		uart1.printf("Task 1 Running!!!\r\n");
-        PB8 = !PB8;
-		os_time_delay(500);
+		if(task1count % 10 == 0)
+		{
+			uart1.printf("------Task 2 Resumed!--------\r\n");
+			os_task_resume(TASK2_PRIO);
+		}
+		os_time_delay(1000);
 	}
 }
 void task_2()
 {
   while(1)
 	{
-        PB9 = !PB9;
-		uart1.printf("Task 2 Running!!!\r\n",task2count);
-		os_time_delay(250);
+		task2count++;
+		uart1.printf("Task 2 Running!!!,runtimes = %d\r\n",task2count);
+		if(task2count%5 == 0)
+		{
+			uart1.printf("------Task 2 suspend!--------\r\n");
+			os_task_suspend(TASK2_PRIO);
+
+		}
+		os_time_delay(1000);
 	}
 
 }
@@ -68,9 +77,8 @@ void task_3()
 {
   while(1)
 	{
-        PB10 = !PB10;
 		uart1.printf("Task 3 Running!!!\r\n");
-		os_time_delay(100);
+		os_time_delay(1000);
 	}
 
 }
