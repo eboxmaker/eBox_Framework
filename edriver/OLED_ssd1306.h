856f8d93faf5c61b5015c52c531b8b95ea5a4980
shentqlf <shentqlf@163.com>
2016-01-23 19:15:26 +0800

    更新oled的API

--- edriver/OLED_ssd1306.h
+++ edriver/OLED_ssd1306.h
@@ -75,26 +75,26 @@
  class OLED_SSD1306
  {
 	 public:
-		OLED_SSD1306(GPIO* p_cs_pin,GPIO* p_res_pin, GPIO* p_dc_pin,GPIO* p_scl_pin,GPIO* p_sda_pin);
+		OLED_SSD1306(GPIO* cs_pin,GPIO* res_pin, GPIO* dc_pin,GPIO* scl_pin,GPIO* sda_pin);
 		void begin();
-		void OLED_Init(void);
-		void OLED_Clear(void);
-		void OLED_Display_On(void);
-		void OLED_Display_Off(void);
+		void init(void);
+		void clear(void);
+		void display_on(void);
+		void display_off(void);
 	 
-		void OLED_ShowChar(uint8_t x,uint8_t y,uint8_t chr);
-		void OLED_ShowString(uint8_t x,uint8_t y, uint8_t *p);	
-		void OLED_ShowNum(uint8_t x,uint8_t y,uint32_t num,uint8_t len,uint8_t size);
+		void show_char(uint8_t x,uint8_t y,uint8_t chr);
+		void show_string(uint8_t x,uint8_t y, uint8_t *p);	
+		void show_num(uint8_t x,uint8_t y,uint32_t num,uint8_t len,uint8_t size);
 	 
-		void OLED_DrawPoint(uint8_t x,uint8_t y,uint8_t t);
-		void OLED_Fill(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint8_t dot);
-		void OLED_ShowCHinese(uint8_t x,uint8_t y,uint8_t no);
-		void OLED_DrawBMP(u16 x0, u16 y0,u16 x1, u16 y1,const unsigned char BMP[]);
+		void draw_point(uint8_t x,uint8_t y,uint8_t t);
+		void fill(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint8_t dot);
+		void show_chinese(uint8_t x,uint8_t y,uint8_t no);
+		void draw_bmp(u16 x0, u16 y0,u16 x1, u16 y1,const unsigned char BMP[]);
 	 
 	 private:
-		void OLED_WrDat(uint8_t dat);	//OLED写数据
-		void OLED_WrCmd(uint8_t cmd);	//OLED写命令
-		void OLED_Set_Pos(uint8_t x, uint8_t y);	// OLED 设置坐标
+		void write_data(uint8_t dat);	//OLED写数据
+		void write_cmd(uint8_t cmd);	//OLED写命令
+		void set_xy(uint16_t x, uint16_t y);	// OLED 设置坐标
 		u32 oled_pow(uint8_t m,uint8_t n);	//m^n
 	 
 		GPIO* cs_pin;	//片选
@@ -103,6 +103,5 @@
 		GPIO* scl_pin;	//D0
 		GPIO* sda_pin;	//D1 
  };
- 
-
 #endif
+
