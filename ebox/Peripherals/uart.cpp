44feaa6fba1f3aaf0e5acbbfc2c4932b3ccfc86e
shentqlf <shentqlf@163.com>
2016-01-25 01:35:05 +0800

    修改串口API，增加串口接受完成中断

--- ebox/Peripherals/uart.cpp
+++ ebox/Peripherals/uart.cpp
@@ -18,8 +18,9 @@ This specification is preliminary and is subject to change at any time without n
 
 
 
-callback_fun_type usart_callback_table[5];//支持串口的rx中断
+callback_fun_type usart_callback_table[5][2];//支持串口的rx中断
 
+uint8_t busy[5];
 
 USART::USART(USART_TypeDef *USARTx,GPIO *tx_pin,GPIO *rx_pin)
 {
@@ -32,64 +33,34 @@ USART::USART(USART_TypeDef *USARTx,GPIO *tx_pin,GPIO *rx_pin)
 void USART::begin(uint32_t baud_rate)
 {
     USART_InitTypeDef USART_InitStructure;
-    NVIC_InitTypeDef NVIC_InitStructure;
 
-    #if (USE_DMA == 1)
+    if(USE_DMA == 1)
         RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);	//使能DMA时钟
-    #endif
 
     switch((uint32_t)_USARTx)
     {
         case (uint32_t)USART1:
             RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
             _DMA1_Channelx = DMA1_Channel4;
-        
-            NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
 
         case (uint32_t)USART2:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
             _DMA1_Channelx = DMA1_Channel7;
-        
-            NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
 
         case (uint32_t)USART3:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3,ENABLE);
             _DMA1_Channelx = DMA1_Channel2;
-        
-            NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
+        
         #if defined (STM32F10X_HD)
         case (uint32_t)UART4:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4,ENABLE);
-        
-            NVIC_InitStructure.NVIC_IRQChannel = UART4_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
+        
         case (uint32_t)UART5:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5,ENABLE);
-        
-            NVIC_InitStructure.NVIC_IRQChannel = UART5_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
         #endif
     }
@@ -102,15 +73,14 @@ void USART::begin(uint32_t baud_rate)
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_Init(_USARTx, &USART_InitStructure);
             
-    if(_USARTx == USART1 | _USARTx == USART2 | _USARTx == USART3 )
+    if((_USARTx == USART1 | _USARTx == USART2 | _USARTx == USART3) && (USE_DMA == 1) )
         USART_DMACmd(_USARTx,USART_DMAReq_Tx,ENABLE);    
     USART_Cmd(_USARTx, ENABLE);
+    interrupt(ENABLE);
 }
 void USART::begin(uint32_t baud_rate,uint8_t data_bit,uint8_t parity,float stop_bit)
 {
     USART_InitTypeDef USART_InitStructure;
-    NVIC_InitTypeDef NVIC_InitStructure;
-
     if (USE_DMA == 1)
         RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);	//使能DMA时钟
 
@@ -119,53 +89,25 @@ void USART::begin(uint32_t baud_rate,uint8_t data_bit,uint8_t parity,float stop_
         case (uint32_t)USART1:
             RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
             _DMA1_Channelx = DMA1_Channel4;
-        
-            NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
 
         case (uint32_t)USART2:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
             _DMA1_Channelx = DMA1_Channel7;
-        
-            NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
 
         case (uint32_t)USART3:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3,ENABLE);
             _DMA1_Channelx = DMA1_Channel2;
-        
-            NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
+        
         #if defined (STM32F10X_HD)
         case (uint32_t)UART4:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4,ENABLE);
-        
-            NVIC_InitStructure.NVIC_IRQChannel = UART4_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
+        
         case (uint32_t)UART5:
             RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5,ENABLE);
-        
-            NVIC_InitStructure.NVIC_IRQChannel = UART5_IRQn;
-            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-            NVIC_Init(&NVIC_InitStructure);
             break;
         #endif
     }
@@ -204,29 +146,88 @@ void USART::begin(uint32_t baud_rate,uint8_t data_bit,uint8_t parity,float stop_
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_Init(_USARTx, &USART_InitStructure);
             
-    if(_USARTx == USART1 | _USARTx == USART2 | _USARTx == USART3 )
+    if((_USARTx == USART1 | _USARTx == USART2 | _USARTx == USART3) && (USE_DMA == 1) )
         USART_DMACmd(_USARTx,USART_DMAReq_Tx,ENABLE);    
     USART_Cmd(_USARTx, ENABLE);
 }
 
 void USART::interrupt(FunctionalState enable)
 {
+    NVIC_InitTypeDef NVIC_InitStructure;
+
 	USART_ITConfig(_USARTx,USART_IT_RXNE,enable);
+	USART_ITConfig(_USARTx,USART_IT_TC,enable);
+	USART_ClearITPendingBit(USART1,USART_IT_TC);
+    
+    switch((uint32_t)_USARTx)
+    {
+        case (uint32_t)USART1:        
+            NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+            break;
+
+        case (uint32_t)USART2:
+            NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+            break;
+
+        case (uint32_t)USART3:
+            NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+            break;
+        #if defined (STM32F10X_HD)
+        case (uint32_t)UART4:        
+            NVIC_InitStructure.NVIC_IRQChannel = UART4_IRQn;
+            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+            break;
+        case (uint32_t)UART5:
+            NVIC_InitStructure.NVIC_IRQChannel = UART5_IRQn;
+            NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+            NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+            break;
+        #endif
+    }
+    NVIC_Init(&NVIC_InitStructure);
 }
-void USART::attach_interrupt(void (*callback_fun)(void))
+void USART::attach_rx_interrupt(void (*callback_fun)(void))
 {
 	switch((uint32_t)_USARTx)
 	{
 		case (uint32_t)USART1:
-			usart_callback_table[0] = callback_fun;break;
+			usart_callback_table[0][0] = callback_fun;break;
 		case (uint32_t)USART2:
-			usart_callback_table[1] = callback_fun;break;
+			usart_callback_table[1][0] = callback_fun;break;
 		case (uint32_t)USART3:
-			usart_callback_table[2] = callback_fun;break;
+			usart_callback_table[2][0] = callback_fun;break;
 		case (uint32_t)UART4:
-			usart_callback_table[3] = callback_fun;break;
+			usart_callback_table[3][0] = callback_fun;break;
 		case (uint32_t)UART5:
-			usart_callback_table[4] = callback_fun;break;
+			usart_callback_table[4][0] = callback_fun;break;
+	}
+}
+void USART::attach_tx_interrupt(void (*callback_fun)(void))
+{
+	switch((uint32_t)_USARTx)
+	{
+		case (uint32_t)USART1:
+			usart_callback_table[0][1] = callback_fun;break;
+		case (uint32_t)USART2:
+			usart_callback_table[1][1] = callback_fun;break;
+		case (uint32_t)USART3:
+			usart_callback_table[2][1] = callback_fun;break;
+		case (uint32_t)UART4:
+			usart_callback_table[3][1] = callback_fun;break;
+		case (uint32_t)UART5:
+			usart_callback_table[4][1] = callback_fun;break;
 	}
 }
 uint16_t USART::receive()
@@ -235,7 +236,6 @@ uint16_t USART::receive()
 }
 uint16_t USART::dma_send_string(const char *str,uint16_t length)
 {
-    wait_busy();
     DMA_DeInit(_DMA1_Channelx);   //将DMA的通道1寄存器重设为缺省值
     _DMA1_Channelx->CPAR = (u32)&_USARTx->DR; //外设地址
     _DMA1_Channelx->CMAR = (u32) str; //mem地址
@@ -258,13 +258,13 @@ uint16_t USART::dma_send_string(const char *str,uint16_t length)
 }
 int USART::put_char(uint16_t ch)
 {
+	while(USART_GetFlagStatus(_USARTx, USART_FLAG_TXE) == RESET);//单字节等待，等待寄存器空
 	USART_SendData(_USARTx,ch);
-	wait_busy();
 	return ch;
 }
 void USART::put_string(const char *str,uint16_t length)
 {
-    if((_USARTx == USART1 | _USARTx == USART2 | _USARTx == USART3 ) && (USE_DMA == 1))
+   if((_USARTx == USART1 | _USARTx == USART2 | _USARTx == USART3 ) && (USE_DMA == 1))
     {
         dma_send_string(str,length);
     }
@@ -272,8 +272,8 @@ void USART::put_string(const char *str,uint16_t length)
     {
         while(length--)
         {
-            USART_SendData(_USARTx,*str++);
-            wait_busy();
+           while(USART_GetFlagStatus(_USARTx, USART_FLAG_TXE) == RESET);//单字节等待，等待寄存器空
+           USART_SendData(_USARTx,*str++);
         }
     }
 }
@@ -298,6 +298,7 @@ void USART::printf(const char *fmt,...)
 {
 
 	wait_busy();
+    set_busy();
 	va_list va_params;   
 	va_start(va_params,fmt);   
 	vsprintf(send_buf,fmt,va_params);
@@ -309,7 +310,45 @@ void USART::printf(const char *fmt,...)
 
 void USART::wait_busy()
 {
-	while(USART_GetFlagStatus(_USARTx, USART_FLAG_TC) == RESET);
+    switch((uint32_t)_USARTx)
+    {
+        case (uint32_t)USART1:
+            while(busy[0] == 1);
+            break;
+        case (uint32_t)USART2:
+            while(busy[1] == 1);
+            break;
+        case (uint32_t)USART3:
+            while(busy[2] == 1);
+            break;
+        case (uint32_t)UART4:
+            while(busy[3] == 1);
+            break;
+        case (uint32_t)UART5:
+            while(busy[4] == 1);
+            break;
+    }
+}
+void USART::set_busy()
+{
+    switch((uint32_t)_USARTx)
+    {
+        case (uint32_t)USART1:
+            busy[0] = 1;
+            break;
+        case (uint32_t)USART2:
+            busy[1] = 1;
+            break;
+        case (uint32_t)USART3:
+            busy[2] = 1;
+            break;
+        case (uint32_t)UART4:
+            busy[3] = 1;
+            break;
+        case (uint32_t)UART5:
+            busy[4] = 1;
+            break;    
+    }
 }
 
 
@@ -319,40 +358,80 @@ void USART1_IRQHandler(void)
 {
 	if(USART_GetITStatus(USART1,USART_IT_RXNE)==SET)
 	{
-		usart_callback_table[0]();
+        if(usart_callback_table[0][0] != 0)
+            usart_callback_table[0][0]();
 		USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+	}
+    if(USART_GetITStatus(USART1,USART_IT_TC)==SET)
+	{
+        busy[0] = 0;
+        if(usart_callback_table[0][1] != 0)
+            usart_callback_table[0][1]();		
+		USART_ClearITPendingBit(USART1,USART_IT_TC);
 	}
 }
 void USART2_IRQHandler(void)
 {
 	if(USART_GetITStatus(USART2,USART_IT_RXNE)==SET)
 	{
-		usart_callback_table[1]();
+        if(usart_callback_table[1][0] != 0)
+            usart_callback_table[1][0]();
 		USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+	}
+    if(USART_GetITStatus(USART2,USART_IT_TC)==SET)
+	{
+        busy[1] = 0;
+        if(usart_callback_table[1][1] != 0)
+            usart_callback_table[1][1]();		
+		USART_ClearITPendingBit(USART2,USART_IT_TC);
 	}
 }
 void USART3_IRQHandler(void)
 {
 	if(USART_GetITStatus(USART3,USART_IT_RXNE)==SET)
 	{
-		usart_callback_table[2]();
+        if(usart_callback_table[2][0] != 0)
+            usart_callback_table[2][0]();
 		USART_ClearITPendingBit(USART3,USART_IT_RXNE);
+	}
+    if(USART_GetITStatus(USART3,USART_IT_TC)==SET)
+	{
+        busy[2] = 0;
+        if(usart_callback_table[2][1] != 0)
+            usart_callback_table[2][1]();		
+		USART_ClearITPendingBit(USART3,USART_IT_TC);
 	}
 }
 void UART4_IRQHandler(void)
 {
 	if(USART_GetITStatus(UART4,USART_IT_RXNE)==SET)
 	{
-		usart_callback_table[3]();
+        if(usart_callback_table[3][0] != 0)
+            usart_callback_table[3][0]();
 		USART_ClearITPendingBit(UART4,USART_IT_RXNE);
+	}
+    if(USART_GetITStatus(UART4,USART_IT_TC)==SET)
+	{
+        busy[3] = 0;
+        if(usart_callback_table[3][1] != 0)
+            usart_callback_table[3][1]();		
+		USART_ClearITPendingBit(UART4,USART_IT_TC);
 	}
 }
 void UART5_IRQHandler(void)
 {
 	if(USART_GetITStatus(UART5,USART_IT_RXNE)==SET)
 	{
-		usart_callback_table[4]();
+        if(usart_callback_table[4][0] != 0)
+            usart_callback_table[4][0]();
 		USART_ClearITPendingBit(UART5,USART_IT_RXNE);
+	}
+    if(USART_GetITStatus(UART5,USART_IT_TC)==SET)
+	{
+        busy[4] = 0;
+        if(usart_callback_table[4][1] != 0)
+            usart_callback_table[4][1]();		
+		USART_ClearITPendingBit(UART5,USART_IT_TC);
 	}
 }
 
