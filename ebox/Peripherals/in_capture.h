d3d321a02fb736e3d2fd1616c27cf009804b3763
shentqlf <shentqlf@163.com>
2016-03-15 02:07:41 +0800

    更正输入捕获模式备注

--- ebox/Peripherals/in_capture.h
+++ ebox/Peripherals/in_capture.h
@@ -22,26 +22,11 @@ This specification is preliminary and is subject to change at any time without n
 1.支持TIM2，3，4的ch1,2,3,4.共计12个通道
 2.支持测量周期、频率、高级用法支持测量占空比
 3.定时器计数器最大值为0xffff,为了突破这个限制，
-    在本例程中，如果使用了update溢出中断调用overflow_event_process可以将计数器
-    拓展至2^32。大大提高测量范围，可以实现最高频率（1分频）测量周期低于60s的信号，如果信号长度。
-    如果使用2分频，可测量周期低于120s的信号，如果信号长度。以此类推。
-4.get_captur()的精度补偿值
-    //此处要加一个简单的修正值,此数为测试大致测试结果
-    //分频系数 | 补偿值 | TIM时钟频率 | 测试频率
-    //7200分频 | 0      | 0.01M       |  1hz-100hz
-    //720分频  | 1      | 0.10M       |  100-1K
-    //72分频   | 2      | 1.00M       |  1K
-    //64分频   | 3      | 1.125M      |  1K
-    //36分频   | 5      | 2.00M       |  1K
-    //32分频   | 6      | 2.25M       |  1K
-    //18分频   | 11     | 4.00M       |  1K
-    //16分频   | 11     | 4.50M       |  1K
-    //9分频    | 18     | 8.00M       |  10K
-    //8分频    | 21     | 9.00M       |  10K-50K
-    //4分频    | 42     | 18.0M       |  10K-50K
-    //2分频    | 84     | 36.0M       |  10K-50K
-    //1分频    | 168    | 72.0M       |  10K-50K
-5.关于分频系数和脉冲宽度测量的计算关系，要遵循一个原则：在不溢出的情况下尽量使用低分频系数（高TIM时钟）去检测对象
+    在本例程中，使用了update溢出中断调用tx_overflow_times可以将计数器
+    拓展至2^32。大大提高测量范围，可以实现最高频率（1分频）测量周期低于120s的信号。
+    如果使用2分频，可测量周期低于240s的信号。以此类推。
+4.关于分频系数和脉冲宽度测量的计算关系，要遵循一个原则：在不溢出的情况下尽量使用低分频系数（高TIM时钟）去检测对象
+5.如果使用某个定时器通道用于输入捕获，则该定时器所有通道都必须是输入捕获模式，不能再设置为其他工作模式
 重点：
     在采用低分频系数的时候，可以保证测量精度，但是会增大定时器溢出频率，进而增大cpu开销，
     在采用高分频系数的时候，测量精度较低，但是会降低定时器溢出频率，进而降低cpu开销，
@@ -52,7 +37,7 @@ class IN_CAPTURE
 {
     public:
         IN_CAPTURE(GPIO *capture_pin);
-        void        begin();//使用默认参数，分频系数为1；最大量程为60s
+        void        begin();//使用默认参数，分频系数为1；最大量程为120s
         void        begin(uint16_t prescaler);
         void        set_count(uint16_t count);
         void        set_polarity_falling();
@@ -76,6 +61,6 @@ class IN_CAPTURE
         void        base_init(uint16_t Period,uint16_t Prescaler);
 
         uint16_t    (*_get_capture)(TIM_TypeDef *TIMx);
-        void        (*_set_polarity)(TIM_TypeDef *TIMx,uint16_t TIM_OCPolarity);//设置为下降沿捕获
+        void        (*_set_polarity)(TIM_TypeDef *TIMx,uint16_t TIM_OCPolarity);//设置为下降沿或者上升沿捕获
 };
 #endif
