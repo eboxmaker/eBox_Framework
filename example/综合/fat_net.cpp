d6184a267e54090b22b8d65ee8742d7448928bed
shentqlf <shentqlf@163.com>
2016-03-21 17:40:01 +0800

    增加综合例程

--- example/综合/fat_net.cpp
+++ example/综合/fat_net.cpp
@@ -1,11 +1,21 @@
+/*
+file   : *.cpp
+author : shentq
+version: V1.0
+date   : 2015/7/5
 
+Copyright 2015 shentq. All Rights Reserved.
+*/
+
+//STM32 RUN IN eBox
 #include "ebox.h"
-#include "w5500.h"
 #include "mmc_sd.h"
-#include "ff.h"
 #include "wrapperdiskio.h"
-#include "udp.h"
-extern void attachSDCardToFat(SD *sd);
+#include "ff.h"
+#include "lcd_1.8.h"
+#include "at24c02.h"
+
+//extern void attach_sd_to_fat(SD *sd);
 
 
 static FATFS fs;            // Work area (file system object) for logical drive
@@ -14,110 +24,86 @@ FRESULT res;
 DIR DirObject;       //目录结构
 DWORD free_clust;//空簇，空扇区大小
 
-u8 mac[6] = {0x00, 0x08, 0xdc, 0x11, 0x11, 0x11}; /*定义Mac变量*/
-u8 lip[4] = {192, 168, 1, 111}; /*定义lp变量*/
-u8 sub[4] = {255, 255, 255, 0}; /*定义subnet变量*/
-u8 gw[4] = {192, 168, 1, 1}; /*定义gateway变量*/
 
-u8 ip[6];
+LCD lcd(&PB5, &PB6, &PB4, &PB3, &spi1);
+SD sd(&PB12, &spi2);
+EEPROM ee(&i2c2);
 
-W5500 w5500(PC13, SPI2, PB13, PB14, PB15, PC14, PC15);
-SD sd(PB12, SPI2, PB13, PB14, PB15);
 
-UDP udp1;
-
-uint32_t ret;
-
+u8 ret;
 u8 buf[1024];
-u32 rl;
-float x;
+
+void getSDCardInfo()
+{
+    u32 rl;
+    float x;
+    ret = sd.get_CID(buf);
+    uart1.printf("\r\n========================");
+    uart1.printf("\r\nget CID Info,ret = %d", ret);
+    uart1.printf("\r\n");
+    uart1.printf((const char *)buf);
+
+    rl = sd.get_capacity();
+    uart1.printf("\r\n========================");
+    uart1.printf("\r\n容量 = %d", rl / 1024 / 1024);
+
+
+    res = f_getfree("/", &free_clust, &fss);
+    if(res == FR_OK)
+    {
+        uart1.printf("\r\n该分区空闲扇区数为：%d", (fss->free_clust) * (fss->csize));
+        uart1.printf("\r\n该分区大小为：%dM", (fss->free_clust) * (fss->csize) / 2048);
+        uart1.printf("\r\n该分区空簇数为：%d", free_clust);
+        uart1.printf("\r\n该分区空扇区数为：%d", free_clust * (fss->csize));
+    }
+    else
+        uart1.printf("\r\n获取分区空簇失败,res = %d", res);
+    uart1.printf("\r\n OVER !");
+
+
+}
 void setup()
 {
     ebox_init();
     uart1.begin(9600);
-    ret = sd.begin();
+    lcd.begin(1);
+    ret = sd.begin(3);
     if(!ret)
         uart1.printf("\r\nsdcard init ok!");
-    f_mount(0, &fs);
+    ee.begin(100000);
+    
+    lcd.clear(BLACK);
 
-    w5500.begin(mac, lip, sub, gw);
-
-    attachEthToSocket(&w5500);
-    attachSDCardToFat(&sd);
-
-    w5500.getMAC (ip);
-    uart1.printf("\r\nmac : %02x.%02x.%02x.%02x.%02x.%02x\r\n", ip[0], ip[1], ip[2], ip[3], ip[4], ip[5]);
-    w5500.getIP (ip);
-    uart1.printf("IP : %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
-    w5500.getSubnet(ip);
-    uart1.printf("mask : %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
-    w5500.getGateway(ip);
-    uart1.printf("GW : %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
-    uart1.printf("Network is ready.\r\n");
-
-    if(udp1.begin(0, 30000) == 0)
-        uart1.printf("\r\nudp1 server creat ok! listen on 30000");
-
-
-    uart1.printf("\r\nret = %d", ret);
-    ret = sd.getCID(buf);
-    uart1.printf("\r\nret = %d", ret);
-    uart1.printf((const char *)buf);
-
-    ret = sd.getCSD(buf);
-    uart1.printf("\r\nret = %d", ret);
-
-    rl = sd.getCapacity();
-    x = (float)rl;
-    uart1.printf("\r\n容量 = %f", x / 1024 / 1024);
-
-    res = f_getfree("/", &free_clust, &fss);
-    uart1.printf("该分区所有扇区数为：%d\r\n", (fss->max_clust - 2) * (fss->csize));
-    if(res == FR_OK)
-    {
-        uart1.printf("该分区所有扇区数为：%d\r\n", (fss->max_clust - 2) * (fss->csize));
-        uart1.printf("该分区大小为：%dM\r\n", (fss->max_clust - 2) * (fss->csize) / 2048);
-        uart1.printf("该分区空簇数为：%d\r\n", free_clust);
-        uart1.printf("该分区空扇区数为：%d\r\n", free_clust * (fss->csize));
-    }
-    else
-        uart1.printf("获取分区空簇失败\r\n,res = %d", res);
-    uart1.printf("\r\n");
+    attach_sd_to_fat(&sd);
+    res = f_mount(&fs, "0:", 1);
+    uart1.printf("\r\nres = %d", res);
+    
 
 }
 u32 count;
 int main(void)
 {
     setup();
+    getSDCardInfo();
     while(1)
     {
-        res = f_opendir(&DirObject, "/123"); //打开目录
-        if(res == FR_OK)
-        {
-            uart1.printf("\r\n打开目录成功 !\r\n");
-            uart1.printf("该目录所在簇号：%d\r\n", DirObject.clust);
-            uart1.printf("该目录所在扇区号：%d\r\n", DirObject.sect);
-        }
-        else if(res == FR_NO_PATH)
-            uart1.printf("所找目录路径不存在\r\n");
-        else
-            uart1.printf("打开目录失败~~~~(>_<)~~~~ \r\n");
-        if(udp1.recv(buf))
-        {
-            uart1.printf("\r\n============================");
-            uart1.printf("\r\n本地端口:%d", udp1.localPort );
-            uart1.printf("\r\n消息来源:%d.%d.%d.%d:%d", udp1.remoteIP[0], udp1.remoteIP[1], udp1.remoteIP[2], udp1.remoteIP[3], udp1.remotePort);
-            uart1.printf("\r\n数据内容:");
-            uart1.printf((const char *)buf);
-            uart1.printf("\r\n============================");
-            udp1.sendto(udp1.remoteIP, udp1.remotePort, buf, 100);
-        }
+
         delay_ms(1000);
     }
 
+}
+
+int ee_check()
+{
 
 }
 
+int w25x16_check()
+{
 
+}
+int sd_check()
+{
 
+}
 
--- user/main.cpp
+++ user/main.cpp
@@ -9,75 +9,148 @@ Copyright 2015 shentq. All Rights Reserved.
 
 //STM32 RUN IN eBox
 #include "ebox.h"
+#include "mmc_sd.h"
+#include "wrapperdiskio.h"
+#include "ff.h"
+#include "lcd_1.8.h"
+#include "at24c02.h"
+#include "w25x16.h"
 #include "ESP8266.h"
-#include "esp8266_tcp.h"
+#include "w5500.h"
 
 
-WIFI_TCP tcp(&wifi);
+int ee_check();
+int w25x16_check();
+int sd_check();
+int wifi_check();
 
 
-char        remote_ip[] = "192.168.1.101";
-uint16_t    remote_port = 8080;
-uint16_t    local_port = 4321;
+static FATFS fs;            // Work area (file system object) for logical drive
+FRESULT res;
+DWORD free_clust;//ø’¥ÿ£¨ø’…»«¯¥Û–°
 
 
-uint8_t recv_buf[1024] = {0};
-uint8_t send_buf[] = "this is a udp send test!\r\n";
-uint16_t len = 0;
-uint32_t count = 0;
+LCD lcd(&PB5, &PB6, &PB4, &PB3, &spi1);
+SD sd(&PB12, &spi2);
+EEPROM ee(&i2c2);
+W25X flash(&PA15, &spi1);
+W5500 w5500(&PC13, &PC14, &PC15, &spi2);
+
+int ret;
+uint8_t wbuf[512];
+uint8_t rbuf[512];
+
 
 void setup()
 {
     ebox_init();
-    uart1.begin(115200);
-    uart1.printf("esp8266 tcp single client test\r\n");
-    uart1.printf("-------------------------------\r\n");
-
+    uart1.begin(9600);
+    lcd.begin(1);
+    flash.begin(2);
+    sd.begin(3);
+    ee.begin(400000);
     wifi.begin(&PA4, &uart2, 115200);
-    wifi.join_ap();
-    tcp.begin();
+    
+    lcd.clear(BLACK);
+    attach_sd_to_fat(&sd);
+    attach_eth_to_socket(&w5500);
 
 }
-
+u32 count;
 int main(void)
 {
-    bool ret;
     setup();
-
-    ret = tcp.connect(remote_ip, remote_port, local_port);
-    if(ret)
-    {
-        uart1.printf("connect ok!\r\n");
-    }
+    
+    ret = ee_check();
+    if(ret == 1)
+        uart1.printf("eeprom check ......[err]\r\n");
     else
-    {
-        uart1.printf("connect failed!\r\n");
-		
-    }
+        uart1.printf("eeprom check ......[OK]\r\n");
+    delay_ms(500);
+    ret = w25x16_check();
+    if(ret == 1)
+        uart1.printf("w25x16 check ......[err]\r\n");
+    else
+        uart1.printf("w25x16 check ......[OK]\r\n");
+    delay_ms(500);
+    ret = sd_check();
+    if(ret == 1)
+        uart1.printf("sdcard check ......[err]\r\n");
+    else
+        uart1.printf("sdcard check ......[OK]\r\n");
+    delay_ms(500);
+        
+    ret = wifi_check();
+    if(ret == 1)
+        uart1.printf("wifi check ........[err]\r\n");
+    else
+        uart1.printf("wifi check ........[OK]\r\n");
+    delay_ms(500);
+    
 
     while(1)
     {
-        len = tcp.read((uint8_t *)recv_buf);
-        if(len)
-        {
 
-            uart1.printf_length((char *)recv_buf, len);
-
-        }
-        if(count == 0)
-        {
-            ret = tcp.send((uint8_t *)send_buf, sizeof(send_buf));
-            if(ret)
-                uart1.printf("send ok!\r\n");
-        }
-        count++;
-        count %= 500000;
-
+        delay_ms(1000);
     }
 
 }
 
+int ee_check()
+{
+    for(uint16_t i = 0; i < 256; i++)
+    {
+        wbuf[i] = random() % 256;
+    }
+    ee.write_byte(0, wbuf, 256);
+    ee.read_byte(0, rbuf, 256);
+    for(int i = 0; i < 256; i++)
+    {
+        if(wbuf[i] != rbuf[i])
+        {
+            ret = 1;
+            break;
+        }
+    }
+
+    return ret;
+}
+
+int w25x16_check()
+{
+    for(uint16_t i = 0; i < 256; i++)
+    {
+        wbuf[i] = random() % 256;
+    }
+    flash.write(wbuf, 0, 256);
+    flash.read(rbuf, 0, 256);
+    for(int i = 0; i < 256; i++)
+    {
+        if(wbuf[i] != rbuf[i])
+        {
+            ret = 1;
+            break;
+        }
+    }
+    return ret;
+
+}
+int sd_check()
+{
+    res = f_mount(&fs, "0:", 1);
+    return res;
+}
+int wifi_check()
+{
+    ret = wifi.kick();
+    if(ret)
+        return 0;
+    else
+        return 1;
+}
+
+void w5500_check()
+{
 
 
-
-
+}
