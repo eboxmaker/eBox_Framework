618b36df43e2aab1fb78272129db9b1783c66f31
shentqlf <shentqlf@163.com>
2016-04-19 01:56:03 +0800

    更新dhcp例程

--- example/14.network/5.dhcp.cpp
+++ example/14.network/5.dhcp.cpp
@@ -14,18 +14,16 @@ Copyright 2015 shentq. All Rights Reserved.
 #include "w5500.h"
 #include "socket.h"
 #include "dhcp.h"
-
+#include "dns.h"
 u8 mac[6] = {0x01,0x01,0x01,0x01,0x01,0x01};
 
 
-u8 ip[6];
 u8 buf[100];
 
 W5500 w5500(&PC13, &PC14, &PC15, &spi2);
-
-DHCP dhcp;
 void setup()
 {
+    int ret;
     ebox_init();
     uart1.begin(115200);
     uart1.printf("\r\nuart1 9600 ok!");
@@ -33,20 +31,23 @@ void setup()
     w5500.begin(2, mac);
     attach_eth_to_socket(&w5500);
 
-    dhcp.begin(mac);
-    w5500.setSHAR(dhcp.net.mac);/*≈‰÷√Macµÿ÷∑*/
-    w5500.setSIPR(dhcp.net.ip);/*≈‰÷√Ipµÿ÷∑*/
-    w5500.setSUBR(dhcp.net.subnet);/*≈‰÷√◊”Õ¯—⁄¬Î*/
-    w5500.setGAR(dhcp.net.gw);/*≈‰÷√ƒ¨»œÕ¯πÿ*/
-    w5500.getMAC (ip);
-    uart1.printf("\r\nmac : %02x.%02x.%02x.%02x.%02x.%02x\r\n", ip[0], ip[1], ip[2], ip[3], ip[4], ip[5]);
-    w5500.getIP (ip);
-    uart1.printf("IP : %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
-    w5500.getSubnet(ip);
-    uart1.printf("mask : %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
-    w5500.getGateway(ip);
-    uart1.printf("GW : %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
-    uart1.printf("Network is ready.\r\n");
+    ret = dhcp.begin(mac);
+    if(ret == EOK)
+    {
+        w5500.setSHAR(dhcp.net.mac);/*≈‰÷√Macµÿ÷∑*/
+        w5500.setSIPR(dhcp.net.ip);/*≈‰÷√Ipµÿ÷∑*/
+        w5500.setSUBR(dhcp.net.subnet);/*≈‰÷√◊”Õ¯—⁄¬Î*/
+        w5500.setGAR(dhcp.net.gw);/*≈‰÷√ƒ¨»œÕ¯πÿ*/
+        w5500.getMAC (buf);
+        uart1.printf("\r\nmac : %02x.%02x.%02x.%02x.%02x.%02x\r\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+        w5500.getIP (buf);
+        uart1.printf("IP : %d.%d.%d.%d\r\n", buf[0], buf[1], buf[2], buf[3]);
+        w5500.getSubnet(buf);
+        uart1.printf("mask : %d.%d.%d.%d\r\n", buf[0], buf[1], buf[2], buf[3]);
+        w5500.getGateway(buf);
+        uart1.printf("GW : %d.%d.%d.%d\r\n", buf[0], buf[1], buf[2], buf[3]);
+        uart1.printf("Network is ready.\r\n");
+    }
 
 }
 u16 len;
