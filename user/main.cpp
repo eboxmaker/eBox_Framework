122e90bbb6db3dd600af1ad3480e6974a57fcfc7
shentqlf <shentqlf@163.com>
2016-05-25 20:04:32 +0800

    修改输入捕获get_zone_time_us()返回值为float类型

--- user/main.cpp
+++ user/main.cpp
@@ -1,49 +1,43 @@
 /*
 file   : *.cpp
 author : shentq
-version: V1.1
+version: V1.0
 date   : 2015/7/5
 
 Copyright 2015 shentq. All Rights Reserved.
 */
 
-//STM32 RUN IN eBox
-
-
+/*
+输入捕获实验-测量周期和频率
+本例程为使用输入捕获模式测量一个PWM信号的周期和频率
+请将PA0和PB6使用跳线链接起来
+*/
 #include "ebox.h"
 
-void rtcsecit()
+IN_CAPTURE ic0(&PA0);//创建一个输入捕获的对象
+PWM pwm1(&PB6);//创建一个PWM输出对象
+
+
+uint32_t us;
+
+
+void m_frq_0()//输入捕获中断事件
 {
-   uart1.printf("count = %x\r\n", rtc.get_counter());
+    us = ic0.get_zone_time_us();//获取两个边沿之间的时间。
 }
-void alarm_event()
-{
-    uart1.printf("alarm event\r\n");
-}
-void overflow_event()
-{
-    uart1.printf("overflow event\r\n");
-}
+
+uint16_t p;
 void setup()
 {
     ebox_init();
     uart1.begin(115200);
-    uart1.printf("uart ok\r\n");
-    
-    if(rtc.begin(1) == EOK)//如果设置成功，则使用外部晶振，如果设置失败则使用内部RC震荡器    
-        uart1.printf("LSE\r\n");
-    else
-        uart1.printf("LEI\r\n");
-    
-    rtc.set_counter(0xfffffffa);
-    rtc.set_alarm(5);
-    rtc.attach_sec_interrupt(rtcsecit);
-    rtc.attach_alarm_interrupt(alarm_event);
-    rtc.attach_overflow_interrupt(overflow_event);
-    
-    rtc.sec_interrupt(ENABLE);
-    rtc.alarm_interrupt(ENABLE);
-    rtc.overflow_interrupt(ENABLE);
+
+    ic0.begin(1);//初始化输入捕获参数，p分频
+    ic0.attch_ic_interrupt(m_frq_0);//绑定捕获中断事件函数
+
+    pwm1.begin(1000, 900);
+    pwm1.set_oc_polarity(1);
+
 }
 
 int main(void)
@@ -51,9 +45,17 @@ int main(void)
     setup();
     while(1)
     {
+        while(us == 0);//等待产生数据
+        uart1.printf("PWM周期 = %dus\r\n", us); //输出PWM周期
+        uart1.printf("PWM频率 = %0.1fhz\r\n", 1000000.0/ (us)); //输出PWM频率
+        us = 0;
         delay_ms(1000);
     }
 }
+
+
+
+
 
 
 
