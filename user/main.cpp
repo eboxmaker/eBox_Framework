e7c13e2100fecb27fe7dedc02074ee93206fd088
DESKTOP-2G67VB9\shentq <shentqlf@163.com>
2016-11-28 23:19:37 +0800

    【优化】micros的一个bug

--- user/main.cpp
+++ user/main.cpp
@@ -1,73 +1,59 @@
-/**
-  ******************************************************************************
-  * @file    pwm.cpp
-  * @author  shentq
-  * @version V1.2
-  * @date    2016/08/14
-  * @brief   ebox application example .
-  ******************************************************************************
-  * @attention
-  *
-  * No part of this software may be used for any commercial activities by any form 
-  * or means, without the prior written consent of shentq. This specification is 
-  * preliminary and is subject to change at any time without notice. shentq assumes
-  * no responsibility for any errors contained herein.
-  * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
-  ******************************************************************************
-  */
+/*
+file   : *.cpp
+author : shentq
+version: V1.0
+date   : 2015/7/5
 
+Copyright 2015 shentq. All Rights Reserved.
+*/
 
-/* Includes ------------------------------------------------------------------*/
+//STM32 RUN IN eBox
 
 
 #include "ebox.h"
-#include "math.h"
+#include "dht11.h"
 
-Pwm pwm1(&PA11);
-Pwm pwm2(&PA1);
+Dht11 sensor(&PB2);
 
 void setup()
 {
     ebox_init();
     uart1.begin(115200);
-    uart1.printf("core:%d\r\n",cpu.clock.core);
-    uart1.printf("core:%d\r\n",cpu.clock.core);
-    uart1.printf("hclk:%d\r\n",cpu.clock.hclk);
-    uart1.printf("pclk1:%d\r\n",cpu.clock.pclk1);
-    uart1.printf("pclk2:%d\r\n",cpu.clock.pclk2);
-    
-    pwm1.begin(1000, 900);
-    pwm1.set_oc_polarity(1);
-    pwm2.begin(1000, 800);
-    pwm2.set_oc_polarity(1);
 
-    
-    uart1.printf("max frq = %dKhz\r\n",pwm1.get_max_frq()/1000);
-    uart1.printf("max frq = %f\r\n",pwm1.get_accuracy());
-    uart1.printf("max frq = %dKhz\r\n",pwm2.get_max_frq()/1000);
-    uart1.printf("max frq = %f\r\n",pwm2.get_accuracy());
-    PB9.mode(OUTPUT_PP);
 }
-
-float x;
-uint16_t y;
-
 int main(void)
 {
     setup();
 
-    
+    uart1.printf("DHT11 TEST PROGRAM \r\n");
+    uart1.printf("LIBRARY VERSION: ");
+    uart1.printf(Dht11::VERSION);
+    uart1.printf("\r\n");
     while(1)
     {
-        PB9.toggle();
-//        x = x + PI * 0.01;
-//        if(x >= PI)x = 0;
-//        y = 2000 - (sin(x) + 1) * 1000;
+        switch (sensor.read())
+        {
+        case Dht11::OK:
+            uart1.printf("Humidity: %d\%% \r\n", sensor.getHumidity());
 
-//        pwm1.set_duty(y);
-        delay_ms(10);
+            uart1.printf("Temperature: %d'C \r\n ", sensor.getTemperature());
+            break;
+
+        case Dht11::ERROR_CHECKSUM:
+            uart1.printf("Checksum error\r\n");
+            break;
+
+        case Dht11::ERROR_TIMEOUT:
+            uart1.printf("Timeout error\r\n");
+            break;
+
+        default:
+            uart1.printf("Unknown error\r\n");
+            break;
+        }
+
+        delay_ms(1000);
     }
-
 }
 
 
