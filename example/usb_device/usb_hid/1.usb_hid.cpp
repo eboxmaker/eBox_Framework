7a52c4e2c1ee4429818ff8ef0d933291666f1f3a
DESKTOP-2G67VB9\shentq <shentqlf@163.com>
2016-12-20 23:05:24 +0800

    【增加】1、usb hid例程和调试工具；2、增加串口调试工具

--- example/usb_device/usb_hid/1.usb_hid.cpp
+++ example/usb_device/usb_hid/1.usb_hid.cpp
@@ -10,13 +10,37 @@ Copyright 2016 link. All Rights Reserved.
 //STM32 RUN IN eBox
 #include "ebox.h"
 #include "USBHID.h"
-
+#include "bsp.h"
+//We declare a USBHID device. By default input and output reports are 64 bytes long.
 USBHID hid;
+
 HID_REPORT recv;
+
+void set_led(uint8_t data)
+{
+		if(data & 0x01)
+		{
+			led1.on();
+		}
+		else
+		{
+			led1.off();
+		}
+		if(data & 0x02)
+		{
+			led2.on();
+		}
+		else
+		{
+			led2.off();
+		}
+}
 
 void setup()
 {
     ebox_init();
+		led1.begin();
+		led2.begin();
 }
 int main(void)
 {
@@ -24,8 +48,8 @@ int main(void)
     while(1)
     {
 			hid.read(&recv);
+			set_led(recv.data[0]);
     }
-
 }
 
 
