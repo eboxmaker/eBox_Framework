81493ab7c8a475c58be1030d738656741531eaf4
DESKTOP-2G67VB9\shentq <shentqlf@163.com>
2017-12-16 21:14:11 +0800

    【增加】lwip2.0.3

---
+++ common/eth/enc28j60.h
@@ -0,0 +1,19 @@
+#ifndef __EtherEnc28j60_H
+#define __EtherEnc28j60_H
+#include "ebox.h"
+
+#include "ether.h"
+
+class EtherEnc28j60 :public Ether
+{
+public:
+    /**
+     * Constructor that uses the default hardware SPI pins
+     * @param cs the Arduino Chip Select / Slave Select pin (default 10 on Uno)
+     */
+    EtherEnc28j60(int8_t id)
+    {};
+
+};
+
+#endif
---
+++ common/eth/udpSocket.h
@@ -0,0 +1,19 @@
+#ifndef __UDPSocket_H
+#define __UDPSocket_H
+#include "ebox.h"
+#include "../eth/ether.h"
+
+class UDPSocket
+{
+public:
+
+    /**
+     * Construct a UDP socket
+     * The local port number will be sent to a random port number
+     *
+     * @param ether The Ethernet interface to attach the socket to
+     */
+    UDPSocket(Ether &ether){};
+};
+
+#endif
