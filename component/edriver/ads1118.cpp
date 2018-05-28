d1c72fd0fdb59e9d45bd15c86052cc88416b8c34
DESKTOP-2G67VB9\shentq <995207301@qq.com>
2018-05-28 11:11:22 +0800

    【修改】ads1118驱动

--- component/edriver/ads1118.cpp
+++ component/edriver/ads1118.cpp
@@ -32,15 +32,33 @@ void Ads1118::begin(uint8_t dev_num)
     cs->mode(OUTPUT_PP);
     cs->set();
     
-    temp_cfg.value = set_cfg(&cfg);
+    temp_cfg = update_cfg(&cfg);
     if(temp_cfg.value != cfg.value)
     {
         uart1.printf("config failed\r\n");
     }
 }
+bool Ads1118::self_test()
+{
+    AdsConfig_t temp_cfg = cfg;
+    if(update_cfg(&temp_cfg).value == temp_cfg.value)
+    {
+        return true;
+    }
+    else
+        return false;
+}
 uint16_t Ads1118::read(uint8_t ch)
 {
     uint16_t value;
+    //更新配置
+    if(cfg.bit.mux != ch)
+    {
+        cfg.bit.mux = ch;
+        update_cfg(&cfg);
+    }
+    
+    //读取相应通道值
     spi->take_spi_right(&config);
     cs->reset();
     while(miso->read() == 1);
@@ -51,22 +69,34 @@ uint16_t Ads1118::read(uint8_t ch)
     spi->release_spi_right();
     return value;
 }
-uint16_t Ads1118::set_cfg(AdsConfig_t *cfg)
+AdsConfig_t Ads1118::update_cfg(AdsConfig_t *cfg)
 {
     
-    uint16_t value;
+    AdsConfig_t temp;
     spi->take_spi_right(&config);
     cs->reset();
     while(miso->read() == 1);
     //delay_us(1);
     spi->write(cfg->byte[0]);
     spi->write(cfg->byte[1]);
-    value |= spi->read() << 8;
-    value |= spi->read();
+    temp.byte[0] = spi->read();
+    temp.byte[1] = spi->read();
     
     cs->set();
     spi->release_spi_right();
-    return value;
+    return temp;
+}
+float Ads1118::read_voltage(uint8_t ch)
+{
+    uint16_t temp;
+    float gain;
+    float voltage;
+    temp = read(ch);
+    float gain_table[8] = {6.144, 4.096, 2.048, 1.024, 0.512, 0.256, 0.256, 0.256};
+    gain = gain_table[cfg.bit.pga];
+    //temp -=32768;
+    voltage = (float)temp*gain/(float)32768;
+    return voltage;
 }
 
 void Ads1118::read_temperature()
