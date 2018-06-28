c4535ea6c954fe4e374d5e6d83bca75702674568
eboxmaker <shentqlf@163.com>
2018-06-28 18:17:04 +0800

    【优化】分离

---
+++ ebox/core/ebox_cppfun.h
@@ -0,0 +1,15 @@
+#ifndef __EBOX_CPPFUN_H
+#define __EBOX_CPPFUN_H
+
+
+#include "../core/stdFun/new.h"
+#include "../core/stdFun/print.h"
+#include "../core/stdFun/ringbuf.h"
+#include "../core/stdFun/list.h"
+#include "../core/stdFun/random.h"
+#include "../core/stdFun/sort.h"
+#include "../core/stdFun/Dstring.h"
+#include "../core/stdFun/Wstring.h"
+
+
+#endif
