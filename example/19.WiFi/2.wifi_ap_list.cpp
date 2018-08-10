6a37dac01aa864c203e8b1b377af692f4eceb2de
eboxmaker <995207301@qq.com>
2018-08-11 00:41:22 +0800

    【修正】ESP8266驱动和例程

--- example/19.WiFi/2.wifi_ap_list.cpp
+++ example/19.WiFi/2.wifi_ap_list.cpp
@@ -14,13 +14,11 @@ Copyright 2015 shentq. All Rights Reserved.
 
 
 /**
-	*	1	此例程演示ESP8266的初始化和版本信息打印
+	*	1	此例程演示如何打印出可用的WiFi列表
 	*	2	此例需要在工程中添加ESP8266.cpp,esp8266_upd.cpp,esp8266_tcp.cpp
 	*/
 /* 定义例程名和例程发布日期 */
-
-
-#define EXAMPLE_NAME	"esp8266 basic test example"
+#define EXAMPLE_NAME	"esp8266 get ap list example"
 #define EXAMPLE_DATE	"2018-08-11"
 
 char recv_buf[1024] = {0};
@@ -33,7 +31,8 @@ void setup()
     print_log(EXAMPLE_NAME,EXAMPLE_DATE);
 
     wifi.begin(&PA4, &uart2, 115200);
-
+    wifi.get_ap_list((char *)recv_buf);
+    uart1.printf((char *)recv_buf);
 }
 
 int main(void)
@@ -42,20 +41,8 @@ int main(void)
     setup();
 
 
-    ret = wifi.kick();
-    if(ret)
-        uart1.printf("kick ok\r\n");
-    else
-        uart1.printf("kick failed\r\n");
-
-    ret = wifi.get_version(recv_buf);
-    if(ret)
-        uart1.printf("%s\r\n", recv_buf);
-    else
-        uart1.printf("get version failed\r\n");
     while(1)
     {
-
 
 
     }
--- user/main.cpp
+++ user/main.cpp
@@ -1,64 +1,68 @@
- /**
-  ******************************************************************************
-  * @file    main.cpp
-  * @author  shentq
-  * @version V2.2
-  * @date    2018/08/6
-  * @brief   ebox application example .
-  ******************************************************************************
-  * @attention
-  *
-  * No part of this software may be used for any commercial activities by any form 
-  * or means, without the prior written consent of shentq. This specification is 
-  * preliminary and is subject to change at any time without notice. shentq assumes
-  * no responsibility for any errors contained herein.
-  * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
-  ******************************************************************************
-  */
+/*
+file   : *.cpp
+author : shentq
+version: V1.0
+date   : 2015/7/5
 
+Copyright 2015 shentq. All Rights Reserved.
+*/
+
+//STM32 RUN IN eBox
 #include "ebox.h"
 #include "bsp_ebox.h"
+#include "../edriver/wifi/ESP8266.h"
+
 
 /**
-	*	1	此例程实现了模数转换。
-	* 
+	*	1	此例程演示ESP8266的初始化和版本信息打印
+	*	2	此例需要在工程中添加ESP8266.cpp,esp8266_upd.cpp,esp8266_tcp.cpp
 	*/
-
 /* 定义例程名和例程发布日期 */
-#define EXAMPLE_NAME	"Adc example"
-#define EXAMPLE_DATE	"2018-08-06"
 
-Adc adc(ADC1);
+
+#define EXAMPLE_NAME	"esp8266 basic test example"
+#define EXAMPLE_DATE	"2018-08-11"
+
+char recv_buf[1024] = {0};
+
+
 void setup()
 {
     ebox_init();
-    UART.begin(115200);
+    uart1.begin(115200);
     print_log(EXAMPLE_NAME,EXAMPLE_DATE);
 
-    adc.add_ch(&PA0);
-    adc.add_ch(&PA1);
-    adc.add_ch(&PA2);
-    adc.add_temp_senser();
-    adc.begin();
+    wifi.begin(&PA4, &uart2, 115200);
+
 }
+
 int main(void)
 {
+    bool ret;
     setup();
+
+
+    ret = wifi.kick();
+    if(ret)
+        uart1.printf("kick ok\r\n");
+    else
+        uart1.printf("kick failed\r\n");
+
+    ret = wifi.get_version(recv_buf);
+    if(ret)
+        uart1.printf("%s\r\n", recv_buf);
+    else
+        uart1.printf("get version failed\r\n");
     while(1)
     {
-        UART.printf("========按照IO查询====================\r\n");
-        UART.printf("adc0:0x%x\t(%0.1fmv)\r\n",adc.read(&PA0),adc.read_voltage(&PA0));
-        UART.printf("adc1:0x%x\t(%0.1fmv)\r\n",adc.read(&PA1),adc.read_voltage(&PA1));
-        UART.printf("adc2:0x%x\t(%0.1fmv)\r\n",adc.read(&PA2),adc.read_voltage(&PA2));
-        UART.printf("adc temp:\t(%0.1f℃)\r\n",adc.read_temp_senser());
-        UART.printf("========按照索引顺序====================\r\n");
-        UART.printf("adc0:0x%x\t(%0.1fmv)\r\n",adc.read(0),adc.read_voltage(0));
-        UART.printf("adc1:0x%x\t(%0.1fmv)\r\n",adc.read(1),adc.read_voltage(1));
-        UART.printf("adc2:0x%x\t(%0.1fmv)\r\n",adc.read(2),adc.read_voltage(2));
-        UART.printf("adc temp:\t(%0.1f℃)\r\n",adc.read_temp_senser());
-        delay_ms(1000);
+
+
+
     }
+
 }
+
+
 
 
 
