d511f9e8920ede12e48e267f06c6eeb81518657d
quli <cat_li8953@qq.com>
2018-08-13 22:00:46 +0800

    timer1，timer2 例程添加说明

--- example/6.timer/1.timer1.cpp
+++ example/6.timer/1.timer1.cpp
@@ -22,6 +22,12 @@
 #include "ebox.h"
 #include "bsp_ebox.h"
 
+/**
+  *	1	此例程演示了timer定时中断，使用tim1
+  *	2	定时器频率为1hz，每秒中断1次
+  * 3   表现为LED闪烁
+  */
+
 /* 定义例程名和例程发布日期 */
 #define EXAMPLE_NAME	"Timer interrupt example"
 #define EXAMPLE_DATE	"2018-08-08"
@@ -45,7 +51,7 @@ Test test;
 void setup()
 {
     ebox_init();
-    uart1.begin(115200);
+    UART.begin(115200);
     print_log(EXAMPLE_NAME,EXAMPLE_DATE);
     
     LED1.mode(OUTPUT_PP);
