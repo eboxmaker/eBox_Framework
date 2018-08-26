235da1fbec05edd61d1df923186913f5e616b84a
eboxmaker <995207301@>
2018-08-26 18:08:10 +0800

    【测试】

--- component/FourAxle/FourAxle.cpp
+++ component/FourAxle/FourAxle.cpp
@@ -29,15 +29,15 @@ uint8_t FourAxle::send1Channel(int16_t value)
     frameStream->write(sendBuffer,7);
     return 7;
 }
-uint8_t FourAxle::sendNChannel(int16_t *buf,uint8_t len)
+uint8_t FourAxle::sendNChannel(uint8_t ch,int16_t *buf,uint8_t len)
 {
     int i=0;
     DataU16_t data;
     
     sendBuffer[i++] = 0xaa;
-    sendBuffer[i++] = 0x55;
-    sendBuffer[i++] = 1;//fun
-    sendBuffer[i++] = 4;//len
+    sendBuffer[i++] = 0xaa;
+    sendBuffer[i++] = ch;//fun
+    sendBuffer[i++] = len*2;//len
     for(int k = 0 ; k < len; k++)
     {
         data.value = *buf++;
@@ -48,6 +48,44 @@ uint8_t FourAxle::sendNChannel(int16_t *buf,uint8_t len)
     frameStream->write(sendBuffer,i+1);
     return i+1;
 }
+uint8_t FourAxle::sendNMStatues(NiMingStatus_t &status)
+{
+    int i = 0;
+    
+    Data16_t tempi16;
+    DataU32_t tempu32;
+    sendBuffer[i++] = 0xaa;
+    sendBuffer[i++] = 0xaa;
+    sendBuffer[i++] = 1;//fun
+    sendBuffer[i++] = 12;//len
+
+    tempi16.value = status.roll;
+    sendBuffer[i++] = tempi16.byte[1];
+    sendBuffer[i++] = tempi16.byte[0];
+    
+    tempi16.value = status.pitch;
+    sendBuffer[i++] = tempi16.byte[1];
+    sendBuffer[i++] = tempi16.byte[0];
+    
+    tempi16.value = status.yaw;
+    sendBuffer[i++] = tempi16.byte[1];
+    sendBuffer[i++] = tempi16.byte[0];
+    
+    tempu32.value = status.alt;
+    sendBuffer[i++] = tempu32.byte[3];
+    sendBuffer[i++] = tempu32.byte[2];
+    sendBuffer[i++] = tempu32.byte[1];
+    sendBuffer[i++] = tempu32.byte[0];
+    
+    sendBuffer[i++] = status.mode;
+    sendBuffer[i++] = status.armed;
+    
+    addSum(sendBuffer);
+    frameStream->write(sendBuffer,i+1);
+    return i+1;
+}
+
+
 uint8_t FourAxle::addSum(uint8_t *buf)
 {
     uint8_t len = getFrameLength(buf);
