a0c648b3b211596b51c2f1d8f4798a2131fd1148
eboxmaker <995207301@>
2018-09-06 21:48:16 +0800

    【增加】ADS1278驱动

---
+++ user/ads1271.cpp
@@ -0,0 +1,26 @@
+#include "ads1271.h"
+
+void ADS1271::begin()
+{
+    config.dev_num  = spi->get_new_dev_num();
+    config.bit_order = MSB_FIRST;
+    config.mode     = SPI_MODE0;
+    config.prescaler = SPI_CLOCK_DIV2;
+    
+    spi->begin(&config);
+    
+    
+}
+int32_t ADS1271::read()
+{
+    uint32_t temp = 0;
+    temp |= spi->read()<<16;
+    temp |= spi->read()<<8;
+    temp |= spi->read();
+    return temp;
+}
+void ADS1271::process()
+{
+    
+}
+
