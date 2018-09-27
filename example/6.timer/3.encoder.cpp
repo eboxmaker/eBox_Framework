572536dee1ad0c097b526cdcd314c0a900a9ca8d
quli <cat_li8953@qq.com>
2018-09-27 22:08:36 +0800

    移植encoder部分，可以编译，无法运行

--- example/6.timer/3.encoder.cpp
+++ example/6.timer/3.encoder.cpp
@@ -1,28 +1,29 @@
 /**
   ******************************************************************************
-  * @file    pwm.cpp
+  * @file    *.cpp
   * @author  shentq
-  * @version V2.0
+  * @version V1.2
   * @date    2016/08/14
-  * @brief   ebox application example .
+  * @brief   
   ******************************************************************************
   * @attention
   *
+  * Copyright 2015 shentq. All Rights Reserved.
+  *
+  * Copyright Notice
   * No part of this software may be used for any commercial activities by any form 
-  * or means, without the prior written consent of shentq. This specification is 
-  * preliminary and is subject to change at any time without notice. shentq assumes
-  * no responsibility for any errors contained herein.
-  * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
+  * or means, without the prior written consent of shentq.
+  *
+  * @Disclaimer
+  * This specification is preliminary and is subject to change at any time without
+  * notice. shentq assumes no responsibility for any errors contained herein.
   ******************************************************************************
   */
 
 
 /* Includes ------------------------------------------------------------------*/
 
-
 #include "ebox.h"
-#include "math.h"
-#include "ebox_encoder.h"
 #include "bsp_ebox.h"
 
 /**
@@ -35,10 +36,18 @@
 #define EXAMPLE_DATE	"2018-08-11"
 
 
-Encoder encoder(TIM4,&PB6,&PB7);
+Encoder encoder(TIM1,&PA8,&PA9);
 float x;
 uint16_t y;
-Pwm pwm1(&PA0);
+Pwm pwm1(TIM3CH1);
+
+//InCapture ic0(&PA0);//创建一个输入捕获的对象
+////Pwm pwm1(&PB6);//创建一个PWM输出对象
+InCapture ic0(TIM1CH2);
+
+uint32_t frq =0;
+
+uint16_t p;
 void setup()
 {
     ebox_init();
@@ -46,11 +55,12 @@ void setup()
     print_log(EXAMPLE_NAME,EXAMPLE_DATE);
 
     encoder.begin(3);
-    pwm1.begin(1000, 1000);
+    pwm1.begin(1000, 500);
     pwm1.set_oc_polarity(1);//set output polarity after compare
     UART.printf("max frq = %dKhz\r\n",pwm1.get_max_frq()/1000);
-    UART.printf("max frq = %f\r\n",pwm1.get_accuracy());
+//    UART.printf("max frq = %f\r\n",pwm1.get_accuracy());
 }
+
 int main(void)
 {
     setup();
@@ -64,10 +74,15 @@ int main(void)
         pwm1.set_duty(y);
         speed = encoder.read_speed()/50;
         UART.printf("count :%0.2f(%d)\r\n",speed,encoder.read_direction());
-        delay_ms(100);
-        
+
+        delay_ms(1000);
+
     }
 }
+
+
+
+
 
 
 
