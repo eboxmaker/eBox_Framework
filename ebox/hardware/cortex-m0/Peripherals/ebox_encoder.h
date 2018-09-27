bd9e5b8a74ac02e8bdf8d97e2b0949f566252d2c
quli <cat_li8953@qq.com>
2018-09-27 22:08:36 +0800

    移植encoder部分，可以编译，无法运行

--- ebox/hardware/cortex-m0/Peripherals/ebox_encoder.h
+++ ebox/hardware/cortex-m0/Peripherals/ebox_encoder.h
@@ -19,30 +19,59 @@
 #define __EBOX_ENCODER_H
 
 #include "ebox_core.h"
+#include "ebox_timer.h"
 #include "mcu.h"
 
-
-class Encoder
+class Encoder:T_base
 {
     public:
-        Encoder(TIM_TypeDef *timer,Gpio *a,Gpio *b);
-        Encoder(TIM_TypeDef *timer,Gpio *a);
-        bool begin(uint32_t pulse_per_circle,uint8_t mode = TIM_EncoderMode_TI12);
+        Encoder(TIM_TypeDef *timer,Gpio *a,Gpio *b):T_base(timer){
+            //this->TIMx = timer;
+            this->a = a;
+            this->b = b;
+        };
+        Encoder(TIM_TypeDef *timer,Gpio *a):T_base(timer){
+            this->a = a;
+        };
+        bool begin(uint32_t pulse_per_circle,uint8_t mode = LL_TIM_ENCODERMODE_X4_TI12);
         double read_speed();//单位：pulse/second;
         uint16_t read_counter();
         bool read_direction();
 
     private:
         void base_init(uint16_t period, uint16_t prescaler);
-        void set_encoder(uint8_t mode = TIM_EncoderMode_TI12);
+        void set_encoder(uint8_t mode = LL_TIM_ENCODERMODE_X4_TI12);
 
     private:
-        TIM_TypeDef *TIMx;
+//        TIM_TypeDef *TIMx;
         Gpio        *a;
         Gpio        *b;    
-        uint16_t    period;
+//        uint16_t    period;
         uint64_t    last_read_speed_time;
         uint32_t    pulse_per_circle;
 };
+
+//class Encoder
+//{
+//    public:
+//        Encoder(TIM_TypeDef *timer,Gpio *a,Gpio *b);
+//        Encoder(TIM_TypeDef *timer,Gpio *a);
+//        bool begin(uint32_t pulse_per_circle,uint8_t mode = TIM_EncoderMode_TI12);
+//        double read_speed();//单位：pulse/second;
+//        uint16_t read_counter();
+//        bool read_direction();
+
+//    private:
+//        void base_init(uint16_t period, uint16_t prescaler);
+//        void set_encoder(uint8_t mode = TIM_EncoderMode_TI12);
+
+//    private:
+//        TIM_TypeDef *TIMx;
+//        Gpio        *a;
+//        Gpio        *b;    
+//        uint16_t    period;
+//        uint64_t    last_read_speed_time;
+//        uint32_t    pulse_per_circle;
+//};
 
 #endif
