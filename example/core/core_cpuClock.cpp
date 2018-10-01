cde3fd7a3407f5775039bb763752d76d5012b554
quli <cat_li8953@qq.com>
2018-10-01 13:07:12 +0800

    添加例程 core_cpuClock 演示如何修改时钟。 core_systickmultipe & core_systick演示如何使用systemtick中断

--- example/core/core_cpuClock.cpp
+++ example/core/core_cpuClock.cpp
@@ -13,12 +13,20 @@
 #include "ebox.h"
 #include "bsp_ebox.h"
 
+/**
+	*	1 此例程演示了如何修改mcu的时钟，以及在不同频率下cpu的运算能力变化
+    *   2 通过SystemClock_Config可以根据需要修改mcu时钟源，该函数系统会自动调用，无需手动调用
+    *   3 f0可以通过st官方的cubeMx软件生成，在project->setting->advanced setting,选择LL即可
+    *   4 f1系列因为使用标准库，无法使用cubeMx软件生成，system_stm32f10x.c中有些默认配置，可以
+    *     通过宏定义选择
+	*/
+
+
 /* 定义例程名和例程发布日期 */
 #define EXAMPLE_NAME	"STM32 cpuClock example"
 #define EXAMPLE_DATE	"2017-12-31"
 
 #ifdef F0
-
 #include "stm32f0xx_ll_utils.h"
 #include "stm32f0xx_ll_rcc.h"
 
@@ -34,65 +42,65 @@
 
 void SystemClock_Config(void)
 {
-	/* Configuration will allow to reach a SYSCLK frequency set to 24MHz:
-	 Syst freq = ((HSI_VALUE * PLLMUL)/ PLLDIV)
-							 ((8MHz * 12)/ 2)                  = 48MHz             */
-	LL_UTILS_PLLInitTypeDef sUTILS_PLLInitStruct = {CPUCLOCK};
-	/* Variable to store AHB and APB buses clock configuration */
-	/* Settings to have HCLK set to 48MHz and APB to 48 MHz */
-	LL_UTILS_ClkInitTypeDef sUTILS_ClkInitStruct = {LL_RCC_SYSCLK_DIV_1, LL_RCC_APB1_DIV_1};
+  /* Configuration will allow to reach a SYSCLK frequency set to 24MHz:
+   Syst freq = ((HSI_VALUE * PLLMUL)/ PLLDIV)
+  						 ((8MHz * 12)/ 2)                  = 48MHz             */
+  LL_UTILS_PLLInitTypeDef sUTILS_PLLInitStruct = {CPUCLOCK};
+  /* Variable to store AHB and APB buses clock configuration */
+  /* Settings to have HCLK set to 48MHz and APB to 48 MHz */
+  LL_UTILS_ClkInitTypeDef sUTILS_ClkInitStruct = {LL_RCC_SYSCLK_DIV_1, LL_RCC_APB1_DIV_1};
 
-	/* Switch to PLL with HSI as clock source             */
-	LL_PLL_ConfigSystemClock_HSI(&sUTILS_PLLInitStruct, &sUTILS_ClkInitStruct);
+  /* Switch to PLL with HSI as clock source             */
+  LL_PLL_ConfigSystemClock_HSI(&sUTILS_PLLInitStruct, &sUTILS_ClkInitStruct);
 }
 #else
 void SystemClock_Config(void)
 {
-     // 可以直接修改system_stm32f10x.c中的宏定义修改频率，也可以在这里定义代码
+  // f1系列可以直接修改system_stm32f10x.c中的宏定义修改频率，也可以在这里定义代码
 }
 #endif
 
 void setup()
 {
-    ebox_init();
-	UART.begin(115200);
-    print_log(EXAMPLE_NAME,EXAMPLE_DATE);
-    LED1.mode(OUTPUT_PP);
+  ebox_init();
+  UART.begin(115200);
+  print_log(EXAMPLE_NAME,EXAMPLE_DATE);
+  LED1.mode(OUTPUT_PP);
 }
 
 void testUs()
 {
-	uint8_t i = 255;
-	while(i--){
-				LED1.toggle();
-			delay_us(1);
-	}
+  uint8_t i = 255;
+  while (i--){
+    LED1.toggle();
+    delay_us(1);
+  }
 }
 
 void testMs()
 {
-	uint8_t i = 20;
-	while(i--){
-				LED1.toggle();
-			delay_ms(1);
-	}
+  uint8_t i = 20;
+  while (i--){
+    LED1.toggle();
+    delay_ms(1);
+  }
 }
 
 int main(void){
-		uint32_t t;
-    setup();
-		UART.printf("启动耗时：%d us,  %d ms \r\n",micros(),millis());
-    while (1)
-    {
-			 t = micros();
-			 testUs();
-			 t = micros() - t;
-			 UART.printf("testUs 耗时：%dus \r\n",t);
-			 // 使用millis_seconds,减少调用消耗
-			 t = millis_seconds;
-			 testMs();
-			 t = millis_seconds - t;
-			 UART.printf("testMs 耗时：%dms \r\n",t);
-             delay_ms(5000);
-    }
+  uint32_t t;
+  setup();
+  UART.printf("启动耗时：%d us,  %d ms \r\n",micros(),millis());
+  while (1)
+  {
+    t = micros();
+    testUs();
+    t = micros() - t;
+    UART.printf("testUs 耗时：%dus \r\n",t);
+    // 使用millis_seconds,减少调用消耗
+    t = millis_seconds;
+    testMs();
+    t = millis_seconds - t;
+    UART.printf("testMs 耗时：%dms \r\n",t);
+    delay_ms(5000);
+  }
 }
