6b384957915d48a427547224f1fb913e57c570b1
quli <cat_li8953@qq.com>
2018-11-04 15:38:51 +0800

    统一f0,f1接口，并完善例程说明。修复f0设置闹铃时分钟可能超过60的bug，修改f1中星期计算函数，1-7标识周一到周日，和f0保持一致

--- ebox/hardware/cortex-m0/Peripherals/ebox_rtc.cpp
+++ ebox/hardware/cortex-m0/Peripherals/ebox_rtc.cpp
@@ -1,13 +1,15 @@
 /**
   ******************************************************************************
-  * @file    ebox_rtc.h
-  * @author  cat_li
+  * @file    rtc.cpp
+  * @author  shentq
+  * @version V2.1
+  * @date    2016/08/14
   * @brief   STM32F0 @ HAL lib
   ******************************************************************************
   * @attention
   *
-  * No part of this software may be used for any commercial activities by any form 
-  * or means, without the prior written consent of shentq. This specification is 
+  * No part of this software may be used for any commercial activities by any form
+  * or means, without the prior written consent of shentq. This specification is
   * preliminary and is subject to change at any time without notice. shentq assumes
   * no responsibility for any errors contained herein.
   * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
@@ -23,7 +25,7 @@
 
 #if USE_PRINTF
 // 是否打印调试信息, 1打印,0不打印
-#define debug 1
+#define debug 0
 #endif
 
 #if debug
@@ -47,7 +49,14 @@
 /* ck_spre=ck_apre/(SYNC prediv + 1) = 1 Hz */
 #define LSE_SYNCH_PREDIV           ((uint32_t)0x00FF)
 
-fun_noPara_t rtc_callback;//
+static uint32_t rtc_irq_id;
+static rtc_irq_handler irq_handler;
+
+void rtc_irq_init(rtc_irq_handler handler, uint32_t id)
+{
+  irq_handler = handler;
+  rtc_irq_id = id;
+}
 
 /**
   * @brief  等待Time&Date寄存器与RTC(APB)时钟同步
@@ -56,19 +65,19 @@ fun_noPara_t rtc_callback;//
   */
 __INLINE int8_t ebox_WaitForSynchro_RTC(void)
 {
-	uint32_t end = GetEndTime(RTC_TIMEOUT);
-	/* Clear RSF flag */
-	LL_RTC_ClearFlag_RS(RTC);
-	/* Wait the registers to be synchronised */
-	while (LL_RTC_IsActiveFlag_RS(RTC) != 1)
-	{
-		if (IsTimeOut(end,RTC_TIMEOUT))
-		{
-			DEBUG("时钟同步超时\r\n");
-			return ETIMEOUT;
-		}
-	}
-	return EOK;
+  uint32_t end = GetEndTime(RTC_TIMEOUT);
+  /* Clear RSF flag */
+  LL_RTC_ClearFlag_RS(RTC);
+  /* Wait the registers to be synchronised */
+  while (LL_RTC_IsActiveFlag_RS(RTC) != 1)
+  {
+    if (IsTimeOut(end,RTC_TIMEOUT))
+    {
+      DEBUG("时钟同步超时\r\n");
+      return ETIMEOUT;
+    }
+  }
+  return EOK;
 }
 /**
   * @brief  进入赋值模式
@@ -78,19 +87,19 @@ __INLINE int8_t ebox_WaitForSynchro_RTC(void)
   */
 int8_t ebox_Enter_RTC_InitMode(void)
 {
-	uint32_t end = GetEndTime(RTC_TIMEOUT);
-	/* Set Initialization mode */
-	LL_RTC_EnableInitMode(RTC);
-	/* Check if the Initialization mode is set */
-	while (LL_RTC_IsActiveFlag_INIT(RTC) != 1)
-	{
-		if (IsTimeOut(end,RTC_TIMEOUT))
-		{
-			DEBUG("进入赋值模式超时,如果使用LSE,请断电后重新上电！\r\n");
-			return ETIMEOUT;
-		}
-	}
-	return EOK;
+  uint32_t end = GetEndTime(RTC_TIMEOUT);
+  /* Set Initialization mode */
+  LL_RTC_EnableInitMode(RTC);
+  /* Check if the Initialization mode is set */
+  while (LL_RTC_IsActiveFlag_INIT(RTC) != 1)
+  {
+    if (IsTimeOut(end,RTC_TIMEOUT))
+    {
+      DEBUG("进入赋值模式超时,如果使用LSE,请断电后重新上电！\r\n");
+      return ETIMEOUT;
+    }
+  }
+  return EOK;
 }
 
 /**
@@ -113,30 +122,32 @@ int8_t ebox_Exit_RTC_InitMode(void)
   */
 int Rtc::begin(ClockS clock)
 {
-	int ret = ENG;
-	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
-	LL_PWR_EnableBkUpAccess();
-	if (clock)	//LSE
-	{
-		if ((_getTimeFlag() == 0) )	//时钟掉电，需要重新设置
-		{
-				if (_config(clock_lse) != EOK)
-				{					
-					DEBUG("LSE时钟启动失败,使用LSI时钟\r\n");
-					//LL_RTC_BAK_SetRegister(RTC, LL_RTC_BKP_DR1, 0x00);
-					_config(clock_lsi);;
-				}
-		}
-		else	// 时钟保持工作，不需要设置
-		{
-			DEBUG("使用LSE,无需配置时钟,Alarm IT is %d \r\n",LL_RTC_IsEnabledIT_ALRA(RTC));      
-			ret = EOK;
-		}
-	}else{	// 其他两种时钟源VDD掉电后RTC状态不定，所以需要初始化
-		_config(clock_lsi);
-	}
-	_nvic();
-	return ret;
+
+  int ret = ENG;
+  /* Enable PWR and BKP clocks */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_PWR_EnableBkUpAccess();
+
+  if (clock == clock_lse){
+    if ((_getTimeFlag() == 0) )	//时钟掉电，需要重新设置
+    {
+      if (_config(clock_lse) != EOK)
+      {
+        DEBUG("LSE时钟启动失败,使用LSI时钟\r\n");
+        //LL_RTC_BAK_SetRegister(RTC, LL_RTC_BKP_DR1, 0x00);
+        _config(clock_lsi);;
+      }
+    }
+    else	// 时钟保持工作，不需要设置
+    {
+      DEBUG("使用LSE,无需配置时钟,Alarm IT is %d \r\n",LL_RTC_IsEnabledIT_ALRA(RTC));
+      ret = EOK;
+    }
+  }else{	// 其他两种时钟源VDD掉电后RTC状态不定，所以需要初始化
+    _config(clock_lsi);
+  }
+  rtc_irq_init(Rtc::_irq_handler, (uint32_t)this);
+  return ret;
 }
 
 /**
@@ -161,7 +172,7 @@ int Rtc::_config(ClockS clock)
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
   LL_PWR_EnableBkUpAccess();
   /*##-2- Configure LSE/LSI as RTC clock source ###############################*/
-  if (clock)		// 1 LSE
+  if (clock == clock_lse)		// 1 LSE
   {
     // 强制备份域重置
     LL_RCC_ForceBackupDomainReset();
@@ -207,7 +218,7 @@ int Rtc::_config(ClockS clock)
   /* 关闭写保护并进入初始化模式 */
   LL_RTC_DisableWriteProtection(RTC);
   ebox_Enter_RTC_InitMode();
-  
+
   /* 设置时间格式&分频因子 */
   LL_RTC_SetHourFormat(RTC, LL_RTC_HOURFORMAT_24HOUR);
 
@@ -220,6 +231,178 @@ int Rtc::_config(ClockS clock)
   return EOK;
 }
 
+/**
+  *@brief    检查时钟是否配置过
+  *@param    none
+  *@retval   0 不支持或未配置
+  */
+uint8_t Rtc::_getTimeFlag(void)
+{
+#if defined(RTC_BACKUP_SUPPORT)
+  return (LL_RTC_BAK_GetRegister(RTC, LL_RTC_BKP_DR1) == RTC_BKP_DATE_TIME_UPDTATED);
+#else
+  return 0;
+#endif
+}
+
+/**
+ *@brief    备份域写信息,用来识别时钟是否已经配置过
+ *@param    none
+ *@retval   none
+*/
+void Rtc::_setTimeFlag(uint16_t configFlag)
+{
+#if defined(RTC_BACKUP_SUPPORT)
+  /*##-8- Writes a data in a RTC Backup data Register1 #######################*/
+  LL_RTC_BAK_SetRegister(RTC, LL_RTC_BKP_DR1, RTC_BKP_DATE_TIME_UPDTATED);
+#endif
+}
+
+/**
+  *@brief    设置日期
+  *@param    Date_T date 日期
+  *@retval   none
+  */
+void Rtc::setDate(Date_T date)
+{
+  /*##-1- 关闭写保护并进入初始化模式 */
+  LL_RTC_DisableWriteProtection(RTC);
+  ebox_Enter_RTC_InitMode();
+  /* Set Date*/
+  LL_RTC_DATE_Config(RTC, __LL_RTC_CONVERT_BIN2BCD(date.WeekDay), __LL_RTC_CONVERT_BIN2BCD(date.Day), __LL_RTC_CONVERT_BIN2BCD(date.Month), __LL_RTC_CONVERT_BIN2BCD(date.Year));
+
+  /*##-3 退出初始化并打开写保护*/
+  ebox_Exit_RTC_InitMode();
+  LL_RTC_EnableWriteProtection(RTC);
+}
+
+/**
+  *@name     setTime
+  *@brief    设置时间,时间格式为bin
+  *@param    Time_T time 时间
+  *@retval   none
+  */
+void Rtc::setTime(Time_T time)
+{
+  /*##-1- 关闭写保护并进入初始化模式 */
+  LL_RTC_DisableWriteProtection(RTC);
+  ebox_Enter_RTC_InitMode();
+
+  /* Set Time*/
+  LL_RTC_TIME_Config(RTC,__LL_RTC_CONVERT_BIN2BCD(time.Format12_24), __LL_RTC_CONVERT_BIN2BCD(time.Hours), __LL_RTC_CONVERT_BIN2BCD(time.Minutes), __LL_RTC_CONVERT_BIN2BCD(time.Seconds));
+
+  /*##-3 退出初始化并打开写保护*/
+  ebox_Exit_RTC_InitMode();
+  LL_RTC_EnableWriteProtection(RTC);
+
+  _setTimeFlag(0);
+}
+
+/**
+  *@brief    设置闹铃
+  *@param    Time_T time 闹铃时间,mask  ALARMA MASK
+  *@retval   none
+  */
+void Rtc::setAlarm(Time_T alarm,uint32_t mask)
+{
+  /*##-1- 关闭写保护并进入初始化模式 */
+  LL_RTC_DisableWriteProtection(RTC);
+  ebox_Enter_RTC_InitMode();
+  /*##-2- 关闭闹铃 */
+  LL_RTC_ALMA_Disable(RTC);
+  _updateTime(&alarm);
+  /*##-3- 设置闹铃&屏蔽位 */
+  LL_RTC_ALMA_ConfigTime(RTC, __LL_RTC_CONVERT_BIN2BCD(alarm.Format12_24), __LL_RTC_CONVERT_BIN2BCD(alarm.Hours), __LL_RTC_CONVERT_BIN2BCD(alarm.Minutes), __LL_RTC_CONVERT_BIN2BCD(alarm.Seconds));
+  LL_RTC_ALMA_SetMask(RTC, mask);
+
+  /*##-4- 打开闹铃,清除标志位 */
+  LL_RTC_ALMA_Enable(RTC);
+  LL_RTC_ClearFlag_ALRA(RTC);
+  /* Enable IT Alarm */
+  /*##-5 退出初始化并打开写保护*/
+  ebox_Exit_RTC_InitMode();
+  LL_RTC_EnableWriteProtection(RTC);
+}
+
+/**
+  *@brief    开启，关闭闹铃
+  *@param    FunctionalState state ENABLE DISABLE
+  *@retval   none
+  */
+void Rtc::setInterrupt(FunctionalState state,Rtc_IrqType type){
+  LL_RTC_DisableWriteProtection(RTC);
+  /* Clear the Alarm interrupt pending bit */
+  LL_RTC_ClearFlag_ALRA(RTC);
+  /* Enable IT Alarm */
+  _nvic(state);
+  (state == ENABLE) ? LL_RTC_EnableIT_ALRA(RTC) : LL_RTC_DisableIT_ALRA(RTC);
+  LL_RTC_EnableWriteProtection(RTC);
+}
+
+/**
+  *@brief    获取当前日期时间
+  *@param    date_time_t *datetime
+  *@retval   none
+  */
+void Rtc::getDateTime(date_time_t *datetime)
+{
+  datetime->Year 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetYear(RTC));
+  datetime->Month = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetMonth(RTC));
+  datetime->Day 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetDay(RTC));
+  datetime->WeekDay 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetWeekDay(RTC));
+
+  datetime->Hours 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetHour(RTC));
+  datetime->Minutes 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetMinute(RTC));
+  datetime->Seconds 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetSecond(RTC));
+}
+
+/**
+  *@brief    获取当前时间
+  *@param    Time_T *time
+  *@retval   none
+  */
+void Rtc::getTime(Time_T *time)
+{
+  time->Format12_24 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetFormat(RTC));
+  time->Hours 		= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetHour(RTC));
+  time->Minutes 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetMinute(RTC));
+  time->Seconds 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetSecond(RTC));
+}
+
+/**
+  *@brief    获取当前日期
+  *@param    Date_T *date
+  *@retval   none
+  */
+void Rtc::getDate(Date_T *date)
+{
+  date->Year	 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetYear(RTC));
+  date->Month	 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetMonth(RTC));
+  date->Day 	 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetDay(RTC));
+  date->WeekDay = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetWeekDay(RTC));
+}
+
+void Rtc::attach(void (*fptr)(void), Rtc_IrqType type) {
+  if (fptr) {
+    _irq.attach(fptr);
+  }
+}
+
+void Rtc::_irq_handler(uint32_t id, Rtc_IrqType irq_type) {
+  Rtc *handler = (Rtc*)id;
+  handler->_irq.call();
+}
+
+void Rtc::_nvic(FunctionalState state)
+{
+  /* RTC Alarm Interrupt Configuration: EXTI configuration */
+  LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_17);
+  LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_17);
+  /*## Configure the NVIC for RTC Alarm ##################################*/
+  NVIC_SetPriority(RTC_IRQn, 0x0F);
+  (state == ENABLE) ? NVIC_EnableIRQ(RTC_IRQn) : NVIC_DisableIRQ(RTC_IRQn);
+}
+
 void Rtc::_setFormat(uint32_t format){
   LL_RTC_DisableWriteProtection(RTC);
   ebox_Enter_RTC_InitMode();
@@ -230,173 +413,43 @@ void Rtc::_setFormat(uint32_t format){
 }
 
 /**
-  *@brief    检查时钟是否配置过
-  *@param    none
-  *@retval   0 不支持或未配置
+  *@brief    更新时间，防止时，分，秒超范围
+  *@param    Time_T *time
+  *@retval   none
   */
-uint8_t Rtc::_getTimeFlag(void)
-{
-#if defined(RTC_BACKUP_SUPPORT)
-	return (LL_RTC_BAK_GetRegister(RTC, LL_RTC_BKP_DR1) == RTC_BKP_DATE_TIME_UPDTATED);
-#else
-	return 0;
-#endif
+void Rtc::_updateTime(Time_T *time){
+  if (time->Seconds >= 60)
+  {
+    time->Seconds -= 60;
+    time->Minutes += 1;
+  }
+
+  if (time->Minutes >= 60)
+  {
+    time->Minutes -=60;
+    time->Hours += 1;
+  }
+
+  if (time->Hours >= 24) time->Hours -= 24;
 }
-
-/**
- *@brief    备份域写信息,用来识别时钟是否已经配置过
- *@param    none
- *@retval   none
-*/
-void Rtc::_setTimeFlag(void)
-{
-#if defined(RTC_BACKUP_SUPPORT)
-	/*##-8- Writes a data in a RTC Backup data Register1 #######################*/
-	LL_RTC_BAK_SetRegister(RTC, LL_RTC_BKP_DR1, RTC_BKP_DATE_TIME_UPDTATED);
-#endif
-}
-
-/**
- *@brief    设置日期
- *@param    Date_T date 日期
- *@retval   none
-*/
-void Rtc::setDate(Date_T date)
-{
-	/*##-1- 关闭写保护并进入初始化模式 */
-	LL_RTC_DisableWriteProtection(RTC);
-	ebox_Enter_RTC_InitMode();
-	/* Set Date*/
-	LL_RTC_DATE_Config(RTC, __LL_RTC_CONVERT_BIN2BCD(date.WeekDay), __LL_RTC_CONVERT_BIN2BCD(date.Day), __LL_RTC_CONVERT_BIN2BCD(date.Month), __LL_RTC_CONVERT_BIN2BCD(date.Year));
-	
-	/*##-3 退出初始化并打开写保护*/
-	ebox_Exit_RTC_InitMode();	
-	LL_RTC_EnableWriteProtection(RTC);
-}
-
-/**
- *@name     setTime
- *@brief    设置时间
- *@param    Time_T time 时间
- *@retval   none
-*/
-void Rtc::setTime(Time_T time)
-{
-	/*##-1- 关闭写保护并进入初始化模式 */
-	LL_RTC_DisableWriteProtection(RTC);
-	ebox_Enter_RTC_InitMode();
-	
-	/* Set Time*/
-	LL_RTC_TIME_Config(RTC,__LL_RTC_CONVERT_BIN2BCD(time.Format12_24), __LL_RTC_CONVERT_BIN2BCD(time.Hours), __LL_RTC_CONVERT_BIN2BCD(time.Minutes), __LL_RTC_CONVERT_BIN2BCD(time.Seconds));
-	
-	/*##-3 退出初始化并打开写保护*/
-	ebox_Exit_RTC_InitMode();
-	LL_RTC_EnableWriteProtection(RTC);
-	
-	_setTimeFlag();	
-}
-
-/**
- *@name     setAlarm
- *@brief    设置闹铃
- *@param    Time_T time 闹铃时间,mask  ALARMA MASK
- *@retval   none
-*/
-void Rtc::setAlarm(Time_T time,uint32_t mask)
-{
-	/*##-1- 关闭写保护并进入初始化模式 */
-	LL_RTC_DisableWriteProtection(RTC);
-	ebox_Enter_RTC_InitMode();
-	/*##-2- 关闭闹铃 */
-	LL_RTC_ALMA_Disable(RTC);
-  /*##-3- 设置闹铃&屏蔽位 */
-	LL_RTC_ALMA_ConfigTime(RTC, __LL_RTC_CONVERT_BIN2BCD(time.Format12_24), __LL_RTC_CONVERT_BIN2BCD(time.Hours), __LL_RTC_CONVERT_BIN2BCD(time.Minutes), __LL_RTC_CONVERT_BIN2BCD(time.Seconds));
-	LL_RTC_ALMA_SetMask(RTC, mask);
-
-	/*##-4- 打开闹铃,清除标志位 */
-	LL_RTC_ALMA_Enable(RTC);
-	LL_RTC_ClearFlag_ALRA(RTC);
-	/* Enable IT Alarm */
-  /*##-5 退出初始化并打开写保护*/
-	ebox_Exit_RTC_InitMode();
-	LL_RTC_EnableWriteProtection(RTC);
-}
-
-
-void Rtc::_nvic(void)
-{
-	/* RTC Alarm Interrupt Configuration: EXTI configuration */
-	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_17);
-	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_17);
-	/*## Configure the NVIC for RTC Alarm ##################################*/
-	NVIC_SetPriority(RTC_IRQn, 0x0F);
-	NVIC_EnableIRQ(RTC_IRQn);
-}
-
-void Rtc::attach_alarm_interrupt(void (*cb_fun)(void))
-{
-    rtc_callback = cb_fun;
-}
-
-void Rtc::alarmOnOff(FunctionalState state)
-{
-	LL_RTC_DisableWriteProtection(RTC);	
-	/* Clear the Alarm interrupt pending bit */
-	LL_RTC_ClearFlag_ALRA(RTC);
-	/* Enable IT Alarm */
-	if (state == ENABLE)
-		LL_RTC_EnableIT_ALRA(RTC);
-	else
-		LL_RTC_DisableIT_ALRA(RTC);	
-	LL_RTC_EnableWriteProtection(RTC);
-}
-
-void Rtc::getDateTime(date_time_t *datetime)
-{
-	datetime->year 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetYear(RTC));
-	datetime->month = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetMonth(RTC));
-	datetime->date 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetDay(RTC));
-	datetime->week 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetWeekDay(RTC));
-
-	datetime->hour 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetHour(RTC));
-	datetime->min 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetMinute(RTC));
-	datetime->sec 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetSecond(RTC));
-};
-
-void Rtc::getTime(Time_T *time)
-{
-	time->Format12_24 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetFormat(RTC));
-	time->Hours 		= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetHour(RTC));
-	time->Minutes 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetMinute(RTC));
-	time->Seconds 	= __LL_RTC_CONVERT_BCD2BIN(LL_RTC_TIME_GetSecond(RTC));
-}
-
-void Rtc::getDate(Date_T *date)
-{
-	date->Year	 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetYear(RTC));
-	date->Month	 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetMonth(RTC));
-	date->Day 	 = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetDay(RTC));
-	date->WeekDay = __LL_RTC_CONVERT_BCD2BIN(LL_RTC_DATE_GetWeekDay(RTC));
-}
-
 
 extern "C" {
-	void RTC_IRQHandler(void)
-	{
-		/* Get the Alarm interrupt source enable status */
-		if (LL_RTC_IsEnabledIT_ALRA(RTC) != 0)
-		{
-			/* Get the pending status of the Alarm Interrupt */
-			if (LL_RTC_IsActiveFlag_ALRA(RTC) != 0)
-			{
-				/* Alarm callback */
-				rtc_callback();
-				/* Clear the Alarm interrupt pending bit */
-				LL_RTC_ClearFlag_ALRA(RTC);
-			}
-		}
-		/* Clear the EXTI's Flag for RTC Alarm */
-		LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_17);
-	}
+  void RTC_IRQHandler(void)
+  {
+    /* Get the Alarm interrupt source enable status */
+    if (LL_RTC_IsEnabledIT_ALRA(RTC) != 0)
+    {
+      /* Get the pending status of the Alarm Interrupt */
+      if (LL_RTC_IsActiveFlag_ALRA(RTC) != 0)
+      {
+        /* Alarm callback */
+        irq_handler(rtc_irq_id,Sec_Irq);
+        /* Clear the Alarm interrupt pending bit */
+        LL_RTC_ClearFlag_ALRA(RTC);
+      }
+    }
+    /* Clear the EXTI's Flag for RTC Alarm */
+    LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_17);
+  }
 }
 
--- example/8.rtc/rtc_calendar F0.cpp
+++ example/8.rtc/rtc_calendar F0.cpp
@@ -11,13 +11,13 @@
   *	   3 设置时钟
   *	   4 设置闹铃，附加闹铃中断回调函数
 	*		 5 时钟从LSI改为LSE时，需要断电重启一次LSE才生效
-	
+
 	已知bug：LSE时闹铃无效
   ******************************************************************************
   * @attention
   *
-  * No part of this software may be used for any commercial activities by any form 
-  * or means, without the prior written consent of shentq. This specification is 
+  * No part of this software may be used for any commercial activities by any form
+  * or means, without the prior written consent of shentq. This specification is
   * preliminary and is subject to change at any time without notice. shentq assumes
   * no responsibility for any errors contained herein.
   * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
@@ -27,9 +27,19 @@
 #include "ebox.h"
 #include "bsp_ebox.h"
 
+/**
+	*	1 此例程演示了如何设置时间，日期，闹铃
+  * 2 F0内置RTC可以提供年月日，时分秒的运算。 F1仅内置了一个简单定时器，年月日，时分秒是通过软件形式提供。
+  *   每天至少调用依次getTime函数，以更新年月日信息
+  * 3 setAlarm需要提供定时时间和屏蔽位两个参数，默认屏蔽日期和星期。F0中屏蔽位有效，F1中无效
+  * 4 setInterrupt用来开启关闭中断,第二个参数为中断类型，F0中无效，F1中可以分别响应闹铃，秒，计数溢出。默
+      认为闹铃。attach中的irq_type同样默认闹铃，F0无效，F1有效
+  * 5 setDate时，F0中星期由用户指定，F1中星期由软件运算得到。两者有差异
+	*/
+
 /* 定义例程名和例程发布日期 */
-#define EXAMPLE_NAME	"STM32F0 RTC example"
-#define EXAMPLE_DATE	"2017-07-18"
+#define EXAMPLE_NAME	"STM32 RTC example"
+#define EXAMPLE_DATE	"2018-11-04"
 
 // 注意E_RTC没有显示构造函数，默认为无参构造，所以rtc后面不跟括号
 Rtc rtc;
@@ -37,63 +47,59 @@ Rtc rtc;
 // 每分钟输出一次闹铃
 void exit()
 {
-	Time_T time;
-	UART.printf("\n\r\n\r");
-	UART.printf("#######################  闹铃  ##############################\n\r");
-	rtc.getTime(&time);
-	UART.printf("#####                 %2d:%02d:%02d                       ######",time.Hours,time.Minutes,time.Seconds);
-	time.Minutes += 1;
-	rtc.setAlarm(time);
-	UART.printf("\r\n");
-	UART.printf("#############################################################\n\r");
+  Time_T time;
+  UART.printf("\n\r\n\r");
+  UART.printf("#######################  闹铃  ##############################\n\r");
+  rtc.getTime(&time);
+  UART.printf("#####                 %2d:%02d:%02d                       ######",time.Hours,time.Minutes,time.Seconds);
+  time.Minutes += 1;
+  rtc.setAlarm(time);
+  UART.printf("\r\n");
+  UART.printf("#############################################################\n\r");
 }
 
 void setup()
 {
 //	Date_T date = {LL_RTC_WEEKDAY_WEDNESDAY, 13, LL_RTC_MONTH_SEPTEMBER, 16};
 //	Time_T time = {LL_RTC_TIME_FORMAT_AM_OR_24, 22, 56, 1};
-    Date_T date = {1, 13, 10, 18};
-    Time_T time = {0, 22, 56, 1};
+  Date_T date = {0, 4, 11, 18};
+  Time_T time = {0, 22, 56, 1};
 
-	ebox_init();
-	UART.begin(115200);
+  ebox_init();
+  UART.begin(115200);
 
-	print_log(EXAMPLE_NAME,EXAMPLE_DATE);
+  print_log(EXAMPLE_NAME,EXAMPLE_DATE);
 // EOK,初始化成功，并且RTC时间在运行，不需要设置日期，时间。否则需要设置
-	if (rtc.begin(clock_lse) != EOK)
-	{
-		rtc.setDate(date);
-		rtc.setTime(time);
-	}
-	// 设置闹铃
+  if (rtc.begin(clock_lse) != EOK)
+  {
+    rtc.setDate(date);
+    rtc.setTime(time);
+  }
+  // 设置闹铃
 
-	rtc.getTime(&time);
-	time.Minutes += 1;
-	time.Seconds += 0;
-	rtc.setAlarm(time);
-	UART.printf("\n\r 闹铃时间设定为：%2d:%02d:%02d秒",time.Hours,time.Minutes,time.Seconds);
-//	rtc.attach_alarm_interrupt(&exit);
-    rtc.attach(&exit,Alr_Irq);
-    rtc.alarmOnOff(ENABLE);
+  rtc.getTime(&time);
+  time.Minutes += 1;
+  time.Seconds += 0;
+  rtc.setAlarm(time,eRtc_MaskDateWeekDay|eRtc_MaskHours);
+  UART.printf("\n\r 闹铃时间设定为：%2d:%02d:%02d秒",time.Hours,time.Minutes,time.Seconds);
+  rtc.attach(&exit,Alr_Irq);
+  rtc.setInterrupt(ENABLE);
 }
 
 uint8_t Week[7][3] = {{"一"},{"二"},{"三"},{"四"},{"五"},{"六"},{"日"}};
 
 int main(void)
 {
-	// date_time_t 声明在common.h中，包含年月日时分秒星期信息
-	Time_T time;
-    Date_T date;
-	setup();
-	while (1)
-	{
-		// 每30s读取一次时间
-		rtc.getTime(&time);
-        rtc.getDate(&date);
-		UART.printf("\n\r %2d:%02d:%02d秒",time.Hours ,time.Minutes ,time.Seconds);
-		UART.printf("\n\r 20%2d年%02d月%2d日 星期%02d",date.Year,date.Month,date.Day,date.WeekDay);
-		delay_ms(30000);
-	}
+  Time_T time;
+  Date_T date;
+  setup();
+  while (1)
+  {
+    // 每30s读取一次时间
+    rtc.getTime(&time);
+    rtc.getDate(&date);
+    UART.printf("\n\r %2d:%02d:%02d秒",time.Hours ,time.Minutes ,time.Seconds);
+    UART.printf("\n\r 20%2d年%02d月%2d日 星期%02d",date.Year,date.Month,date.Day,date.WeekDay);
+    delay_ms(30000);
+  }
 }
-
-
