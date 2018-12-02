8da932231ff6281760461b561b891b8be67a3056
quli <cat_li8953@qq.com>
2018-12-02 16:24:31 +0800

    添加例程，演示window，button，textbox，checkbox 用法。

--- example/16.gui/4.uGUI_Window.cpp
+++ example/16.gui/4.uGUI_Window.cpp
@@ -3,9 +3,9 @@
 #include "U_GUI.h"
 
 /**
-  *	1	此例程演示了uGUI的基本操作,包括清屏，画矩形，圆，圆弧，圆角矩形，网格，直线。设置字体，输出字符串
+  *	1	此例程演示了uGUI的window，button，textbox，checkbox以及不同字体的调用
 	*	2	依赖lcd_1.8.cpp
-	*	3	分两次循环显示在屏幕上
+	*	3	需要定时调用UG_UPDATE()才能运行这些组件
 	*/
 
 /* 定义例程名和例程发布日期 */
@@ -49,23 +49,29 @@ UG_BUTTON button1_6;
 
 UG_TEXTBOX textbox3_1;
 
+UG_CHECKBOX checkbox1_1;
+
 /* Callback function for the main menu */
 void window_1_callback( UG_MESSAGE* msg )
 {
+	 
    if ( msg->type == MSG_TYPE_OBJECT )
    {
-      if ( msg->id == OBJ_TYPE_BUTTON )
+		 
+      if (msg->id == OBJ_TYPE_BUTTON )
       {
+				 UART.printf("type = %d,id= %d, sid = %d,event = %d \r\n",msg->type,msg->id,msg->sub_id,msg->event);
          switch( msg->sub_id )
-         {
+         {					
             case BTN_ID_0: /* Toggle green LED */
             {
-//               TOGGLE_GREEN_LED;
+							 LED1 = 0;
                break;
             }
             case BTN_ID_1: /* Toggle red LED */
-            {
-//               TOGGLE_RED_LED;
+            {							
+								LED1.toggle();
+							 eGui.CheckboxSetCheched(2);
                break;
             }
             case BTN_ID_2: /* Show 礕UI info */
@@ -112,6 +118,8 @@ void window_1_callback( UG_MESSAGE* msg )
                tog = ! tog;
                break;
             }
+						default:
+							break;
          }
       }
    }
@@ -122,33 +130,50 @@ void window_1_callback( UG_MESSAGE* msg )
 void setup()
 {
 	ebox_init();
+	BtnPin.mode(INPUT_PU);
+	LED1.mode(OUTPUT_PP);
+	UART.begin(115200);
 	lcd.begin();
 	eGui.Begin(&_HW_DrawPixel,(void*)_HW_DrawLine,(void*)_HW_FillFrame,128,160);
 	eGui.FillScreen(C_BLACK);
+	eGui.FontSelect(&FONT_5X12);		// 默认字体
 	eGui.DrawRoundFrame(1,1,127,159,4,C_WHITE);
-	
-   /* -------------------------------------------------------------------------------- */
-   /* Create Window 3 (Benchmark Result)                                               */
-   /* -------------------------------------------------------------------------------- */
+	// create window
 	 eGui.WindowCreate(&window_1, obj_buff_wnd_1, MAX_OBJECTS, window_1_callback);
-	 eGui.WindowSetTitleHeight(0);
-//   UG_WindowSetTitleText( &window_1, "Benchmark Result" );
-//   UG_WindowSetTitleTextFont( &window_1, &FONT_8X12 );
+//eGui.WindowSetTitleHeight(0);
+//eGui.WindowSetStyle(WND_STYLE_2D);		// 2D 没有title，默认为3D
+	 eGui.WindowSetTitleTextFont(&FONT_8X12);	//没有特别指定，使用FontSelect选择的字体
+	 eGui.WindowSetTitleText("ebox uGUI");
+
 
    /* Create Textbox 1 */
-   UG_TextboxCreate( &window_1, &textbox3_1, TXB_ID_0, 5, 10, UG_WindowGetInnerWidth( &window_1 )-5, 60 );
-   UG_TextboxSetFont( &window_1, TXB_ID_0, &FONT_8X12 );
-   UG_TextboxSetText( &window_1, TXB_ID_0, "Result:\n99999 frm/sec" );
-   UG_TextboxSetAlignment( &window_1, TXB_ID_0, ALIGN_TOP_CENTER );
-
+	 
+   eGui.TextboxCreate(&textbox3_1, TXB_ID_0, 5, 10, eGui.WindowGetInnerWidth()-5, 60 );
+//   eGui.TextboxSetFont(TXB_ID_0, &FONT_8X12 );
+	 eGui.TextboxSetText(TXB_ID_0, "Result:\n99999 frm/sec");
+	 eGui.TextboxSetAlignment(TXB_ID_0,ALIGN_TOP_CENTER);
+	 eGui.TextboxDelete(TXB_ID_0,&window_1);
+	 
    /* Create Button 1 */
-   UG_ButtonCreate( &window_1, &button1_1, BTN_ID_0, 40, 65, UG_WindowGetInnerWidth( &window_1 )-40, 100 );
-   UG_ButtonSetFont( &window_1, BTN_ID_0, &FONT_8X12 );
-   UG_ButtonSetText( &window_1, BTN_ID_0, "OK!" );
+   eGui.ButtonCreate(&button1_1, BTN_ID_1, 40, 65, eGui.WindowGetInnerWidth( &window_1 )-40, 90);
+   eGui.ButtonSetFont(&FONT_8X12);
+   eGui.ButtonSetText("OK!" );
+	 //eGui.ButtonHide(BTN_ID_1);
+	 
+	 eGui.ButtonCreate(&button1_2, BTN_ID_0, 40, 95, eGui.WindowGetInnerWidth( &window_1 )-40, 125);
+//   eGui.ButtonSetFont(&FONT_8X12,BTN_ID_0);
+//   eGui.ButtonSetText("N0!",BTN_ID_0,&window_1);
+	 eGui.ButtonSetFont(&FONT_8X12);
+   eGui.ButtonSetText("N0!");
+	 
+	 // 复选框
+	 eGui.CheckboxCreate(&checkbox1_1, TXB_ID_0, 5, 35, UG_WindowGetInnerWidth( &window_1 )-5, 60);
+	 eGui.CheckboxSetFont(&FONT_8X12);
+	 eGui.CheckboxSetText("OK!" );
 
 	 eGui.WindowShow();
 	 attachSystickCallBack(UG_Update,500);	// 每500ms更新一次界面
-	 UG_WaitForUpdate();
+	 eGui.WaitForUpdate();
 }
 int main(void)
 {
@@ -156,5 +181,9 @@ int main(void)
 	while (1)
 	{
 			delay_us(500);
+			if(BtnPin == 0){
+				delay_ms(30);
+				if(BtnPin == 0)	UG_Button_CLICK(&window_1,BTN_ID_1);
+			}
 	}
 }
