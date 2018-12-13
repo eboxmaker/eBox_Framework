e938e8d032c7cc14cadc986be6dc8ee7b8f4f311
eboxmaker <995207301@qq.com>
2018-12-13 18:40:42 +0800

    【增加】辅助工具

---
+++ 0.software_tools/AutoProject/AutoProject/AutoProject/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoProject
+{
+
+    public partial class Form1 : Form
+    {
+        excleOpt excle = new excleOpt();
+        DataTable dt = new DataTable();
+        Target mcu = new Target();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string str = System.AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo file = new FileInfo("McuConfiglist.xlsx");
+            string FileFullPath = file.FullName.ToString();
+
+            dt =  excle.GetExcelToDataTableBySheet(FileFullPath,"Sheet1");
+            for(int i = 0 ; i < dt.Rows.Count; i++)
+            {
+                comboBox1.Items.Add(dt.Rows[i][0]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnUpdataConfigHeader_Click(object sender, EventArgs e)
+        {
+            mcu.updataMcuConfigHeader();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (comboBox1.SelectedItem.ToString() == dt.Rows[i][0].ToString())
+                {
+                    int j = 0;
+                    mcu.name = dt.Rows[i][j++].ToString();
+                    mcu.Package = dt.Rows[i][j++].ToString();
+                    mcu.FlashSzie = Convert.ToInt16(dt.Rows[i][j++]);
+                    mcu.RAMSize = Convert.ToInt16(dt.Rows[i][j++]);
+                    mcu.IoNum = Convert.ToInt16(dt.Rows[i][j++]);
+                    mcu.Freq = Convert.ToInt16(dt.Rows[i][j++]);
+                    mcu.PinNum = mcu.getPinNum();
+                    mcu.FatherType = mcu.parserFatherType();
+
+                    j = 1;
+                    listBox2.Items.Clear();
+                    for (int k = 1; k < dt.Columns.Count; k++)
+                    {
+                        listBox2.Items.Add(dt.Rows[i][j++].ToString());
+                    }
+
+                }
+            }
+
+        }
+
+
+
+    }
+}
---
+++ 0.software_tools/AutoProject/AutoProject/AutoProject/excleOpt.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProject
+{
+    class excleOpt
+    {
+
+        public  DataTable GetExcelToDataTableBySheet(string FileFullPath, string SheetName)
+        {
+
+            //此连接只能操作Excel2007之前(.xls)文件
+
+           // string strConn = "Provider=Microsoft.Jet.OleDb.4.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 8.0; HDR=NO; IMEX=1'";
+
+            //此连接可以操作.xls与.xlsx文件
+
+            string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 12.0; HDR=NO; IMEX=1'";
+
+
+            OleDbConnection conn = new OleDbConnection(strConn);
+
+            conn.Open();
+
+            DataSet ds = new DataSet();
+
+           // OleDbDataAdapter odda = new OleDbDataAdapter(string.Format("SELECT * FROM [{0}]", SheetName), conn);
+
+            OleDbDataAdapter odda = new OleDbDataAdapter(string.Format("select * from [Sheet1$]", conn),conn);             　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
+
+            odda.Fill(ds, SheetName);
+
+            conn.Close();
+
+            return ds.Tables[0];
+
+        }
+
+        //根据Excel物理路径获取Excel文件中所有表名 
+
+        public  String[] GetExcelSheetNames(string excelFile)
+        {
+
+            OleDbConnection objConn = null;
+
+            System.Data.DataTable dt = null;
+
+            try
+            {
+
+                //此连接只能操作Excel2007之前(.xls)文件
+
+                //string strConn = "Provider=Microsoft.Jet.OleDb.4.0;" + "data source=" + excelFile + ";Extended Properties='Excel 8.0; HDR=NO; IMEX=1'";
+
+                //此连接可以操作.xls与.xlsx文件
+
+                string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + excelFile + ";Extended Properties='Excel 12.0; HDR=NO; IMEX=1'";
+
+
+                objConn = new OleDbConnection(strConn);
+
+                objConn.Open();
+
+                dt = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                if (dt == null)
+                {
+
+                    return null;
+
+                }
+
+                String[] excelSheets = new String[dt.Rows.Count];
+
+                int i = 0;
+
+                foreach (DataRow row in dt.Rows)
+                {
+
+                    excelSheets[i] = row["TABLE_NAME"].ToString();
+
+                    i++;
+
+                }
+
+                return excelSheets;
+
+            }
+
+            catch
+            {
+
+                return null;
+
+            }
+
+            finally
+            {
+
+                if (objConn != null)
+                {
+
+                    objConn.Close();
+
+                    objConn.Dispose();
+
+                }
+
+                if (dt != null)
+                {
+
+                    dt.Dispose();
+
+                }
+
+            }
+
+        }
+    }
+}
