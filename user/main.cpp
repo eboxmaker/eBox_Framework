c5238e84be6777a020bbf4d9d1139c25ed94ba2f
eboxmaker <995207301@qq.com>
2019-05-16 11:44:55 +0800

    修改ds3231驱动

--- user/main.cpp
+++ user/main.cpp
@@ -1,95 +1,94 @@
-/**
-  ******************************************************************************
-  * @file    pwm.cpp
-  * @author  shentq
-  * @version V2.0
-  * @date    2016/08/14
-  * @brief   ebox application example .
-  ******************************************************************************
-  * @attention
-  *
-  * No part of this software may be used for any commercial activities by any form
-  * or means, without the prior written consent of shentq. This specification is
-  * preliminary and is subject to change at any time without notice. shentq assumes
-  * no responsibility for any errors contained herein.
-  * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
-  ******************************************************************************
-  */
+/*
+file   : *.cpp
+author : shentq
+version: V1.0
+date   : 2015/7/5
 
+Copyright 2015 shentq. All Rights Reserved.
+*/
 
-/* Includes ------------------------------------------------------------------*/
-
-#include "bsp_ebox.h"
+//STM32 RUN IN eBox
 #include "ebox.h"
-#include "math.h"
-#include "font.h"
-#include "ebox_mem.h"
-#include "gt30l32s4w.h"
-#include "rtservice.h"
+#include "mmc_sd.h"
+#include "wrapperdiskio.h"
+#include "ff.h"
 
 
-#include "t_menu.h"
+static FATFS fs;            // Work area (file system object) for logical drive
+FATFS *fss;
+FRESULT res;
+DIR DirObject;       //目录结构
+DWORD free_clust;//空簇，空扇区大小
 
-//Oled4Spi oled(&PA4, &PA2, &PA1, &PA5, &PA7);
-//FontLib font(&PA8,&PA5,&PA7,&PA6);
 
-uint8_t buf[1024];
-uint8_t buf2[256];
-FontLib font(&PA3,&spi1);
-bool api(uint16_t inner_code,uint8_t font_id,eBoxCharInfo_t *info)
+SD sd(&PB12, &spi2);
+
+void getSDCardInfo()
 {
-    
-    return font.get_data(inner_code,font_id,info);
+    u8 ret;
+    uint64_t rl;
+    u8 buf[1024];
+
+    ret = sd.get_CID(buf);
+    uart1.printf("\r\n========================");
+    uart1.printf("\r\nget CID Info,ret = %d", ret);
+    uart1.printf("\r\n");
+    uart1.printf((const char *)buf);
+
+    rl = sd.get_capacity();
+    uart1.printf("\r\n========================");
+    uart1.printf("\r\ncapacity = %dMB", rl / 1024 / 1024);
+    uart1.printf("\r\ncapacity = %0.1fGB", rl / 1024 / 1024 / 1024.0);
+
+    uart1.printf("\r\nWaiting...");
+    res = f_getfree("/", &free_clust, &fss);
+    if(res == FR_OK)
+    {
+        uart1.printf("\r\npartition size：%dMB", (fss->free_clust) * (fss->csize) / 2048);
+        uart1.printf("\r\npartition free sectors: %d", (fss->free_clust) * (fss->csize));
+        uart1.printf("\r\npartition free clust:%d", free_clust);
+        uart1.printf("\r\npartition free sector:%d", free_clust * (fss->csize));
+    }
+    else
+        uart1.printf("\r\nget capacity faile,err = %d", res);
+    uart1.printf("\r\nOVER !");
+
+
 }
 void setup()
 {
+    u8 ret;
     ebox_init();
     uart1.begin(115200);
-    oled.begin();
-    font.begin();
-    
-    gui.begin(&oled,SSD1322_LCDWIDTH,SSD1322_LCDHEIGHT);
-    gui.set_font_select(FONT_ALL_EXTERN);
-    gui.set_font_hz_extern(Hz_32x32_ID);
-    gui.set_font_ascii_extern(Ascii_16x32_ID);
-    gui.attach(api );
-    gui.fill_screen(0);
-    gui.set_color(GUI_YELLOW);
-    gui.set_font(&GUI_FontHZ16X16);
-    gui.set_text_mode(TEXT_MODE_TRANS);
-    gui.set_rotation(0);
-    side_bar.max = 3;
-    side_bar.prograss = 0;
-    side_bar.orientation = 1;   
-    
-    
-    print_log("氧化锆测试项目",__DATE__);
-    
-    
-    UART.setTimeout(10);
-    LED1.mode(OUTPUT_PP);
-    UART.printf("\r\n");
-    displayCurrentMenu(&menu_demo);
+    ret = sd.begin(3);
+    if(ret == 0)
+        uart1.printf("sdcard init ok!\r\n");
+    else
+        uart1.printf("sdcard init failed;err = %d\r\n", ret);
+
+    attach_sd_to_fat(&sd);
+
+    res = f_mount(&fs, "0", 1);
+    if(res == FR_OK)
+        uart1.printf("mount ok!\r\n", res);
+    else
+        uart1.printf("mount err!err = %d\r\n", res);
+
 }
-
-
-
+u32 count;
 int main(void)
 {
     setup();
-
-  
-
-
-
-while(1)
+    getSDCardInfo();
+    while(1)
     {
 
-
-
-    menu_task(&menu_demo);
-
+        delay_ms(1000);
     }
+
+
 }
+
+
 
 
