4f15d0f11b82e0616c5cfd8c10860d7db4c649bd
eboxmaker <995207301@qq.com>
2019-12-05 04:03:21 +0800

    增加littleFs文件系统的例程

--- user/main.cpp
+++ user/main.cpp
@@ -9,37 +9,49 @@ Copyright 2015 shentq. All Rights Reserved.
 
 //STM32 RUN IN eBox
 #include "ebox.h"
-#include "w25xxx.h"
 #include "mmc_sd.h"
+#include "w25xxx.h"
 #include "bsp_ebox.h"
 #include "fatstructs.h"
 #include "interface/storage/filesystem/dir.h"
 #include "interface/storage/filesystem/file.h"
-//#include "interface/storage/filesystem/littlefs/LittleFileSystem.h"
+#include "interface/storage/filesystem/littlefs/LittleFileSystem.h"
 #include "interface/storage/filesystem/fat/FatFileSystem.h"
+
 
 using namespace ebox;
 
-SD sd(&PB12, &spi2);
-//W25xxx sd(&PA15, &spi1);
+//W25xxx flash(&PA15, &spi1);
+SD flash(&PB12, &spi2);
 
-FATFileSystem fs("fs");
-//LittleFileSystem fs1("fs1");
+LittleFileSystem fs("fs");
 
-#define MAX_BUFFER_SIZE 1
+
+#define MBED_TEST_BUFFER 1024
+
+#define MBED_TEST_TIMEOUT 480
+
+
+
+
+
 
 Dir dir;
+
 File file;
 
-DIR *dd;
-FILE *fd;
+DIR *dd[4];
+FILE *fd[4];
+
+//DIR *dd[MBED_TEST_DIRS];
+//FILE *fd[MBED_TEST_FILES];
 
 struct dirent ent;
 struct dirent *ed;
 size_t size;
-uint8_t buffer[MAX_BUFFER_SIZE];
-uint8_t rbuffer[MAX_BUFFER_SIZE];
-uint8_t wbuffer[MAX_BUFFER_SIZE];
+uint8_t buffer[MBED_TEST_BUFFER];
+uint8_t rbuffer[MBED_TEST_BUFFER];
+uint8_t wbuffer[MBED_TEST_BUFFER];
 
 //int LittleFileSystem::format(BlockDevice *bd,
 //                             lfs_size_t read_size, lfs_size_t prog_size,
@@ -47,78 +59,42 @@ uint8_t wbuffer[MAX_BUFFER_SIZE];
 
 void test_directory_creation()
 {
-    int res = sd.init();
+    int res = flash.init();
     
-//    sd.erase_chip();
-    res = fs.format(&sd,4*1024);
-
-    res = fs.mount(&sd);
-    if(res == FR_OK)
-    {
-        uart1.printf("1mount ok:%d\r\n",res);
-        uart1.flush();
-        res = fs.reformat(&sd,4*1024);
-        
-    }
-    else
-    {
-        uart1.printf("1mount err :%d\r\n",res);
-        uart1.flush();
-        res = fs.mount(&sd);
+    fs.format(&flash,flash.get_read_size(),flash.get_program_size(),
+        flash.get_erase_size(),128);  
+    
+    res = fs.mount(&flash);
+    if (res) {
 
     }
-//    if(res == FR_OK)
-//    {
-//        uart1.printf("reformat ok:%d\r\n",res);
-//    }
-//    else
-//    {
-//        uart1.printf("reformat err :%d\r\n",res);
-//    }
-    uart1.flush();
 
-
-
-
-    res = fs.mkdir("0:potato2", 0777);
-    if(res == FR_OK)
-    {
-        uart1.printf("1mkdir ok:%d\r\n",res);
-    }
-    else
-    {
-        uart1.printf("1mkdir err :%d\r\n",res);
-    }
-    uart1.flush();
+    res = fs.mkdir("potato1", 0777);
 
     res = fs.unmount();
-    if(res == FR_OK)
-    {
-        uart1.printf("1unmount ok:%d\r\n",res);
-    }
-    else
-    {
-            uart1.printf("1unmount err :%d\r\n",res);
-    }
-    uart1.flush();
-
-    res = sd.deinit();
     
-//    while(1);
+    
+    {
+        uart1.printf("init :%d\r\n",res);
+    }
+
+    res = flash.deinit();
+    
 }
 
 void test_root_directory()
 {
-    int res = sd.init();
-    res = fs.mount(&sd);
-//    res = fs.mkdir("/potato2", 0777);
-//    res = fs.mkdir("/potato4", 0777);
+    int res = flash.init();
+    res = fs.mount(&flash);
+    res = fs.mkdir("/potato2", 0777);
+    res = fs.mkdir("/potato4", 0777);
     res = dir.open(&fs, "/");
     struct dirent x;
     int i = 0;
     while(1)
     {
         
+//        dir.seek(i++);
         res = dir.read(&x);
         if(res)
         {
@@ -135,60 +111,28 @@ void test_root_directory()
     
     
     res = fs.unmount();
-    res = sd.deinit();
+    res = flash.deinit();
 }
 
 void test_simple_file_test()
 {
-    int res = sd.init();
+    int res = flash.init();
 
-    res = fs.mount(&sd);
-    if(res == 0)
-    {
-        uart1.printf("file mount ok\r\n");
-    }
-    else
-    {
-        uart1.printf("mount£º[%d]\r\n",res);
-    }    
-    res = file.open(&fs, "/he.txt", O_WRONLY | O_CREAT);
-    if(res == 0)
-    {
-        uart1.printf("creat ok\r\n");
-    }
-    else
-    {
-        uart1.printf("creat err£º[%d]\r\n",res);
-    }
-
+    res = fs.mount(&flash);
+    res = file.open(&fs, "hello.txt", O_WRONLY | O_CREAT);
     size = strlen("Hello World!\n");
     memcpy(wbuffer, "Hello World!\n", size);
     res = file.write(wbuffer, size);
-    uart1.printf("write size:%d\r\n",res);
- 
     res = file.close();
-    
-    
-    res = file.open(&fs, "/he.txt", O_RDONLY);
-    if(res == 0)
-    {
-        uart1.printf("open ok\r\n");
-    }
-    else
-    {
-        uart1.printf("open err£º[%d]\r\n",res);
-    }
-
-    uart1.flush();
+    res = file.open(&fs, "hello.txt", O_RDONLY);
     size = strlen("Hello World!\n");
     res = file.read(rbuffer, size);
-    uart1.printf("read size:%d[%s]\r\n",res,rbuffer);
-
+    uart1.printf("read £º[%s]\r\n",rbuffer);
+//    res = memcmp(rbuffer, wbuffer, size);
     res = file.close();
     res = fs.unmount();
 
-    res = sd.deinit();
-    uart1.flush();
+    res = flash.deinit();
 }
 
 
@@ -199,7 +143,7 @@ void setup()
     uart1.begin(115200);
     print_log();
     test_directory_creation();
-    test_root_directory();
+test_root_directory();
     test_simple_file_test();
 }
 
@@ -210,7 +154,7 @@ int main(void)
     while(1)
     {
 
-        uart1.printf("\r\nrunning£¡");
+        //uart1.printf("\r\nrunning£¡");
         delay_ms(1000);
     }
 
