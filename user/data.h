9071219d833d5bb1d2cc1b603be6bd2a2ab173d1
shentqlf@163.com <>
2020-04-06 23:54:55 +0800

    测试机器学习识别1，2，3，4，5

---
+++ user/data.h
@@ -0,0 +1,18 @@
+#ifndef __DATA_H
+#define __DATA_H
+#include "ebox.h"
+
+
+extern float w1[20][25];
+extern float w2[20][20];
+extern float w3[20][20];
+extern float w4[5][20];
+
+extern float x1[];
+extern float x2[];
+extern float x3[];
+extern float x4[];
+extern float x5[];
+
+#endif
+
